3e538af7cd90734656c0b47c5581a0fb0080fc13
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-01-26 22:25:16 +0000

    added grapth

--- uiAssets/homeTab.php
+++ uiAssets/homeTab.php
@@ -4,34 +4,61 @@
 
     <hr class="mb-0">
 
-    <!-- Accordion card -->
-    <div class="card">
-        <!-- Card header -->
-        <div class="card-header" role="tab" id="heading">
-            <a data-toggle="collapse" data-parent="#accordionEx194" href="#collapse" aria-expanded="true"
-               aria-controls="collapse4">
-                <h3 class="mb-0 mt-3 red-text">
-                    Home ' . $x . ' <i class="fas fa-angle-down rotate-icon fa-2x"></i>
-                </h3>
-            </a>
-        </div>
-        <!-- Card body -->
-        <div id="collapse" class="collapse show" role="tabpanel" aria-labelledby="heading"
-             data-parent="#accordionEx194">
-            <div class="card-body pt-0">
-                <!-- TODO  get test grapth working before implmenting it useing a database-->
-                <?php
-                require "../testLineChart_Year.php"
-                ?>
+    <?php
 
+    //TODO FIND OUT WHAT ELCE SHOULD BE IMPLMENTED
+    //TODO implment lineChart.php after completing it
+    $x = 1;
+    //for building more than one chart
+    $chartId = "chartId" . $x . "";
+
+    while ($x <= 1) {
+        //TODO fix drop dowp fucntion
+        echo '
+        <!-- Accordion card -->
+          <div class="card">
+          <!-- Card header -->
+                <div class="card-header" role="tab" id="heading' . $x . '">
+                  <a data-toggle="collapse" data-parent="#accordionEx194" href="#collapse' . $x . '" aria-expanded="true"
+                    aria-controls="collapse4">
+                    <h3 class="mb-0 mt-3 red-text">
+                      My Home <i class="fas fa-angle-down rotate-icon fa-2x"></i>
+                    </h3>
+                  </a>
+                </div>
+          <!-- Card body -->
+            <div id="collapse' . $x . '" class="collapse show" role="tabpanel" aria-labelledby="heading' . $x . '"
+              data-parent="#accordionEx194">
+                  <div class="card-body pt-0">
+                  
+                  
+                  <div class="flex-sm-row justify-content-center">     
+                      <!--Grid column-->
+                        <!--Date select
+                        <p class="lead align-content-center">
+                          <span class="badge info-color-dark p-2">Date range</span>
+                        </p>-->
+                         <select class="browser-default custom-select">
+                          <option selected>Choose time period</option>
+                          <option value="1">Week</option>
+                          <option value="2">Month</option>
+                          <option value="3">Year</option>
+                        </select>
+                  </div>
+                 <!--TODO intagrate database qurry -->
+                    <canvas id="lineChart"></canvas>
+                  </div>
+                  
             </div>
         </div>
-
-        //TODO FIND OUT WHAT ELCE SHOULD BE IMPLMENTED
-        //TODO implment lineChart.php after completing it
-
-    </div>
-    <!--/.Accordion wrapper-->
-
+      <!-- Accordion card -->
+      ';//echo end
+        $x++;//+1 to x for steting
+    }
+    ?>
 </div>
-<!-- Accordion card -->
+
+<?php
+include "testLineChart_Year.php";
+?>
+<!--/.Accordion wrapper-->

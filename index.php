8c324e37efdf69d8f1197dc4b17852a49cb5bff8
bww1 <bww1@hw.ac.uk>
2020-01-30 02:29:43 +0000

    Fixed

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
     //SETUP REQUIREMENTS
-    require './required/config.php'
+    require './required/config.php';
 
 
     //REQUIRES SESSIONS

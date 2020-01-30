3fd2af1b3b6de714cdc400f3abb9627fa25e2952
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-01-30 20:21:26 +0000

    update

--- account.php
+++ account.php
@@ -5,33 +5,32 @@
 require "uiAssets/userNav.php";
 ?>
 
-        <!--left-->
+<!--left-->
 
-        <div class="card justify-content-center ">
-            <!--Title-->
-            <h4 class="card-title"><a><strong>My Account</strong></a></h4>
-            <!-- Card content -->
-            <div class="card-body ">
-
-
-
-                    <div class="flex-row">
-                        e:mail
-                    </div>
-
-                    <div class="flex-row justify-content-between">
-                        <div class="col-md-6">
-                            firss name
-                        </div>
-                        <div class="col-md-6">
-                            surname
-                        </div>
-                    </div>
+<div class="card justify-content-center ">
+    <!--Title-->
+    <h4 class="card-title"><a><strong>My Account</strong></a></h4>
+    <!-- Card content -->
+    <div class="card-body ">
 
 
-                    </div>
+        <div class="flex-row">
+            e:mail
+        </div>
 
-                </div>
+        <div class="flex-row justify-content-between">
+            <div class="col-md-6">
+                firss name
+            </div>
+            <div class="col-md-6">
+                surname
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
 
 <!-- Card -->
 

a8fc06e13f1aeff78fb86eef9ad60acd2f7eb124
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-02-01 19:38:58 +0000

    removed empty else

--- required/logoutAction.php
+++ required/logoutAction.php
@@ -3,6 +3,6 @@ function logout($Current_SESSION){
     if (isset($Current_SESSION)){
         echo "<a class=\"nav-link\" href=\"index.php\">Log Out</a>";
         session_destroy();
-    }else{};
+    }
 }
 ?>

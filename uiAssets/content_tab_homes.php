60eef33c7d1fb3a849413df0ac4705cc3a04eab6
bww1 <bww1@hw.ac.uk>
2020-02-02 23:17:53 +0000

    Working on home page

--- uiAssets/content_tab_homes.php
+++ uiAssets/content_tab_homes.php
@@ -15,39 +15,37 @@ function generateHomeTab(){
             <div class="card">
             <!-- Card header -->
                 <div class="card-header" role="tab" id="heading' . $x . '">
-                    <a data-toggle="collapse" data-parent="#accordionEx194" href="#collapse' . $x . '" aria-expanded="true"
-                    aria-controls="collapse4">
-                    <h3 class="mb-0 mt-3 red-text">
-                        My Home <i class="fas fa-angle-down rotate-icon fa-2x"></i>
-                    </h3>
+                    <a data-toggle="collapse" data-parent="#accordionEx194" href="#collapse' . $x . '" aria-expanded="true" aria-controls="collapse4">
+                        <h3 class="mb-0 mt-3 red-text">
+                            <div class="row">
+                                <div class="col-auto mr-auto">My Home</div>
+                                <div class="col-auto"><i class="fas fa-angle-down rotate-icon fa-2x"></i></div>
+
+                            </div>
+                        </h3>
                     </a>
                 </div>
-            <!-- Card body -->
-            <div id="collapse' . $x . '" class="collapse show" role="tabpanel" aria-labelledby="heading' . $x . '"
-                data-parent="#accordionEx194">
+
+                <!-- Card body -->
+                <div id="collapse' . $x . '" class="collapse show" role="tabpanel" aria-labelledby="heading' . $x . '" data-parent="#accordionEx194">
                     <div class="card-body pt-0">
-                    
-                    
-                    <div class="flex-sm-row justify-content-center">     
-                        <!--Grid column-->
-                        <!--Date select
-                        <p class="lead align-content-center">
-                            <span class="badge info-color-dark p-2">Date range</span>
-                        </p>-->
-                            <select class="browser-default custom-select">
-                            <option selected>Choose time period</option>
-                            <option value="1">Week</option>
-                            <option value="2">Month</option>
-                            <option value="3">Year</option>
-                        </select>
+                        <div class="flex-sm-row justify-content-center">     
+                            <!--Grid column-->
+                            <!--Date select
+                            <p class="lead align-content-center">
+                                <span class="badge info-color-dark p-2">Date range</span>
+                            </p>-->
+                                <select class="browser-default custom-select">
+                                <option selected>Choose time period</option>
+                                <option value="1">Week</option>
+                                <option value="2">Month</option>
+                                <option value="3">Year</option>
+                            </select>
+                        </div>                        
+                        <canvas id="lineChart_Year"></canvas>
                     </div>
-                    <!--TODO intagrate database qurry -->
-                    
-                    <canvas id="lineChart_Year"></canvas>
-                    </div>
-                    
+                </div>
             </div>
-        </div>
         <!-- Accordion card -->
         ';//echo end
         $x++;//+1 to x for steting

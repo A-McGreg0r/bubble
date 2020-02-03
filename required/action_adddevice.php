3556915d6a33d37e157f7792f20d256cb8dea6e1
bww1 <bww1@hw.ac.uk>
2020-02-03 23:58:07 +0000

    Fixes

--- required/action_adddevice.php
+++ required/action_adddevice.php
@@ -4,7 +4,6 @@
     $imageURI = str_replace(' ','+',$imageURI);
     $uri = substr($imageURI,strpos($imageURI,",")+1);
     $uri = base64_decode($uri);
-    echo $uri;
     session_start();
 
     file_put_contents(dirname(__DIR__).'/upload/'.$_SESSION['user_id'].".png",$uri);

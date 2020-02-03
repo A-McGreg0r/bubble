d57db6f031713197b09d532d785617c727a7f23b
bww1 <bww1@hw.ac.uk>
2020-02-03 23:30:23 +0000

    Fixes

--- js/bubble.js
+++ js/bubble.js
@@ -47,14 +47,13 @@ function submitImage(){
     var img = document.createElement("img");
     img.src = canvas.toDataURL();
     image.prepend(img);
-
+    var dataQuery = img.src;
     loading.style.visibility = "visible";
     $.ajax({
         type:'POST',
         url: url,
-        data:img.src,
+        data:dataQuery,
         cache:false,
-        contentType: false,
         processData: false,
         success:function(data){
             console.log(img.src);

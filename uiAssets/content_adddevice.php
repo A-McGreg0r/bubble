d020516dc9034a25f8a2cc7b286f205619f5198c
bww1 <bww1@hw.ac.uk>
2020-02-03 22:25:37 +0000

    Fixes

--- uiAssets/content_adddevice.php
+++ uiAssets/content_adddevice.php
@@ -20,7 +20,7 @@ function generateQRReader(){
                             <h3>Add new devices by scanning the QR code on the device</h3>
                         </div>
                         <div class="align-middle">
-                            <div style="width: 100% position:absolute "class="text-center align-middle">
+                            <div style="width: 100%; position:absolute;"class="text-center align-middle">
                                 <i class="far fa-circle"></i>
                             </div>
                             <video autoplay="true" id="videoElement" style="width:100%">

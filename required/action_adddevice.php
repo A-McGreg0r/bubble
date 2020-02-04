0904fab6b7398f2141d1fb5d92fbd7ec73b50ee2
bww1 <bww1@hw.ac.uk>
2020-02-04 16:24:19 +0000

    Working on QR reader

--- required/action_adddevice.php
+++ required/action_adddevice.php
@@ -2,7 +2,6 @@
     use Zxing\QrReader;
 
     $imageURI = $_POST['photo'];
-    echo var_dump($_POST);
     $imageURI = str_replace(' ', '+', $imageURI);
 	$imageURI = str_replace('data:image/png;base64,', '', $imageURI);
 
@@ -17,6 +16,7 @@
     $image = dirname(__DIR__).'/upload/'.$_SESSION['user_id'].".png";
 
     $qrcode = new QrReader($image);
-    $this->assertSame("Hello world!", $qrcode->text());
+
+    echo $qrcode->text();
 
 ?>

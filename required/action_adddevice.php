25311ca08761676399e21440ace5f424fe58ac44
bww1 <bww1@hw.ac.uk>
2020-02-04 16:14:53 +0000

    Fixes

--- required/action_adddevice.php
+++ required/action_adddevice.php
@@ -1,4 +1,6 @@
 <?php
+    use Zxing\QrReader;
+
     $imageURI = $_POST['photo'];
     echo var_dump($_POST);
     $imageURI = str_replace(' ', '+', $imageURI);
@@ -10,5 +12,11 @@
     session_start();
 
     file_put_contents(dirname(__DIR__).'/upload/'.$_SESSION['user_id'].".png",$decoded);
+
+
+    $image = dirname(__DIR__).'/upload/'.$_SESSION['user_id'].".png";
+
+    $qrcode = new QrReader($image);
+    $this->assertSame("Hello world!", $qrcode->text());
 
 ?>

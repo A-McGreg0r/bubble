cb4f5bc08c867a0bf181bb3618c3efc691f3c134
bww1 <bww1@hw.ac.uk>
2020-02-04 16:43:26 +0000

    Working on qr code reader

--- required/action_adddevice.php
+++ required/action_adddevice.php
@@ -2,7 +2,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    use qrcode\Zxing\QrReader;
+    require dirname(__DIR__) . "/vendor/autoload.php";
 
     $imageURI = $_POST['photo'];
     $imageURI = str_replace(' ', '+', $imageURI);

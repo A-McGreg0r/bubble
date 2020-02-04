8c906b3af6f8dc65f5070488bc045075e9e036d1
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-02-04 01:05:44 +0000

    test

--- uiAssets/content_tab_homes.php
+++ uiAssets/content_tab_homes.php
@@ -193,9 +193,8 @@ function generateHomeTab(){
                                   
                     <!-- Accordion card -->
 html;
-                    require "charts/lineChart_Day.php";
-                    require "charts/lineChart_Year.php";
-                    require "charts/lineChart_Month.php";
+
+                    require "charts/allCharts.php";
                     $html .= generateLineChart_Day();
                     $html .= generateLineChart_Month();
                     $html .= generateLineChart_Year();

7bb5dbedd68ecd0a2bf52dfdecd958255fe4b0ba
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-02-05 03:57:52 +0000

    charts

--- uiAssets/content_tab_homes.php
+++ uiAssets/content_tab_homes.php
@@ -184,22 +184,10 @@ function generateHomeTab()
     <script>    
     
     
-                var context = document.querySelector('#graph').getContext('2d');
-            new Chart(context).Line(data);
-                
-            $("#btn1").on("click", function() {
-                 var context1 = document.querySelector('#graph').getContext('2d');
-                new Chart(context1).Line(data);
-              });
-            $("#btn2").on("click", function() {
-                var context2 = document.querySelector('#graph').getContext('2d');
-                new Chart(context2).Line(data1);
-              });
-            var data = {
+            let data = {
                 labels: ['January', 'February', 'March'],
                 
-                datasets: [
-                    {
+                datasets: [ {
                     fillColor: "rgba(220,220,220,0.2)",
                     strokeColor: "rgba(220,220,220,1)",
                     pointColor: "rgba(220,220,220,1)",
@@ -207,8 +195,7 @@ function generateHomeTab()
                     pointHighlightFill: "#fff",
                     pointHighlightStroke: "rgba(220,220,220,1)",
                     data: [30,120,90]
-                    },
-                    {
+                    },{
                     fillColor: "rgba(100,220,220,0.7)",
                     strokeColor: "rgba(220,220,220,1)",
                     pointColor: "rgba(220,220,220,1)",
@@ -216,14 +203,11 @@ function generateHomeTab()
                     pointHighlightFill: "#fff",
                     pointHighlightStroke: "rgba(220,220,220,1)",
                     data: [10,70,110]
-                    }
-                ]
-                };
+                    }]};
             
-            var data1 = {
+            let data1 = {
                 labels: ['March', 'Apr', 'May'],
-                datasets: [
-                    {
+                datasets: [ {
                     fillColor: "rgba(220,220,220,0.2)",
                     strokeColor: "rgba(220,220,220,1)",
                     pointColor: "rgba(220,220,220,1)",
@@ -231,8 +215,7 @@ function generateHomeTab()
                     pointHighlightFill: "#fff",
                     pointHighlightStroke: "rgba(220,220,220,1)",
                     data: [50,100,140]
-                    },
-                    {
+                    },{
                     fillColor: "rgba(100,220,220,0.7)",
                     strokeColor: "rgba(220,220,220,1)",
                     pointColor: "rgba(220,220,220,1)",
@@ -240,19 +223,17 @@ function generateHomeTab()
                     pointHighlightFill: "#fff",
                     pointHighlightStroke: "rgba(220,220,220,1)",
                     data: [40,70,200]
-                    }
-                ]
-                };
+                    }]};
             
-            var context = document.querySelector('#graph').getContext('2d');
+            let context = document.querySelector('#graph').getContext('2d');
             new Chart(context).Line(data);
                 
             $("#btn1").on("click", function() {
-                 var context1 = document.querySelector('#graph').getContext('2d');
+                 let context1 = document.querySelector('#graph').getContext('2d');
                 new Chart(context1).Line(data);
               });
             $("#btn2").on("click", function() {
-                var context2 = document.querySelector('#graph').getContext('2d');
+                let context2 = document.querySelector('#graph').getContext('2d');
                 new Chart(context2).Line(data1);
               });
     </script>                

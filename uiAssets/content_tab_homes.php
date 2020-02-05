7bd74a47a7a46a7b374f82c54f2f13da1250618c
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-02-05 05:48:48 +0000

    closer!!

--- uiAssets/content_tab_homes.php
+++ uiAssets/content_tab_homes.php
@@ -180,9 +180,7 @@ function generateHomeTab()
                                     <canvas id="masterLineChart"></canvas>
                                     <script type="text/javascript">
                                     
-                                    const gradientFill = kChartCanvas.createLinearGradient(0, 0, 0, 350);//todo requires intagration?
-                                            gradientFill.addColorStop(0, "rgba(242,38,19,0.5)");
-                                            gradientFill.addColorStop(1, "rgba(0,230,64,0.5)");
+                              
                                         // Supplied Datasets to display
                                         let data1 = { "labels": ["1", "2", "3","4","5","6","7", "8", "9","10","11","12"],"label": "My First dataset", "datasets": [{ "label": "avg", "data": $dataAvgEncoded, "backgroundColor": "rgba(255, 99, 132, 0.2)", "borderColor": "rgba(255,99,132,1)" "borderWidth": 1 },{ "label": "Power usage", "data": $dataPointsEncoded, "backgroundColor": "rgba(255, 99, 132, 0.2)", "borderColor": "rgba(255,99,132,1)", "borderWidth": 1 }] };
                                         let data2 = { "labels": ["1", "2", "3","4","5","6","7", "8", "9","10","11","12"],"label": "My First dataset", "datasets": [{ "label": "avg", "data": $dataAvgEncoded, "backgroundColor": "rgba(255, 99, 132, 0.2)", "borderColor": "rgba(255,99,132,1)", "borderWidth": 1 },{ "label": "Power usage", "data": $dataPointsEncoded, "backgroundColor": "rgba(255, 99, 132, 0.2)", "borderColor": "rgba(255,99,132,1)", "borderWidth": 1 }] };

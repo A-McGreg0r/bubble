d553e626c029551bc3984af8097a5b94c804a833
bww1 <bww1@hw.ac.uk>
2020-02-05 00:17:37 +0000

    Working on add device

--- js/bubble.js
+++ js/bubble.js
@@ -44,7 +44,7 @@ function submitImage(){
     var devicetext = document.querySelector("#devicetext");
 
     var canvas = document.createElement("canvas");
-    canvas.getContext('2d').drawImage(video, 0, 0, image.clientWidth, image.clientHeight);
+    canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
     var img = document.createElement("img");
     img.src = canvas.toDataURL();
     image.prepend(img);

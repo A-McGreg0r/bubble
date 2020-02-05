5cc60b723dcdcea156e78bccbd845a53e775bf37
bww1 <bww1@hw.ac.uk>
2020-02-05 00:19:53 +0000

    Working on add device

--- uiAssets/content_adddevice.php
+++ uiAssets/content_adddevice.php
@@ -25,7 +25,7 @@ function generateQRReader(){
                         </button>
                     </div>
                     <video autoplay="true" id="videoElement" style="width:100%"></video>
-                    <div id="capturedimage" style="width:100%; height:100%; display:none;"></div>
+                    <div id="capturedimage" style="width:100%; height:100%;"></div>
 
                 </div>
             </div>

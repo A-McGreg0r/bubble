8ff32ef9a4cb3f5665ec0b562e171015b6a19e2a
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-02-05 04:38:49 +0000

    chart....

--- uiAssets/content_tab_homes.php
+++ uiAssets/content_tab_homes.php
@@ -170,7 +170,7 @@ function generateHomeTab()
                                     <canvas id="masterLineChart"></canvas>
                                     <script type="text/javascript">
                                         // Supplied Datasets to display
-                                        var data1 = { "labels": ["1", "2", "3"], "datasets": [{ "label": "Contacts", "data": [20, 15, 10, 20, 15, 1020, 15, 10], "backgroundColor": "rgba(255, 99, 132, 0.2)", "borderColor": "rgba(255,99,132,1)", "borderWidth": 1 }] };
+                                        var data1 = { "labels": ["1", "2", "3","4","5"], "datasets": [{ "label": "Contacts", "data": [20, 15, 10, 20, 15, 1020, 15, 10], "backgroundColor": "rgba(255, 99, 132, 0.2)", "borderColor": "rgba(255,99,132,1)", "borderWidth": 1 }] };
                                         var data2 = { "labels": ["1", "2", "3"], "datasets": [{ "label": "Contacts", "data": [10, 23, 41], "backgroundColor": "rgba(255, 99, 132, 0.2)", "borderColor": "rgba(255,99,132,1)", "borderWidth": 1 }] };
                                         
                                         // Draw the initial chart

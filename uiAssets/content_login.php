9daadae99a6dd70d0545173855e83d18093315ae
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-02-05 08:53:53 +0000

    loging update

--- uiAssets/content_login.php
+++ uiAssets/content_login.php
@@ -16,7 +16,7 @@ function generateLoginPage(){
     $html = <<<htmlPage
     <!--Modal cascading tabs-->
     <div class="container justify-content-center max-width:80%">
-        <table style="height: 100vh; " class=" text-centre">
+        <table style="height: 100vh; " class="text-centre text-light">
             <tr>
                 <td class="align-middle">
                     <div class="text-center">

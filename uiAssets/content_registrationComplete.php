7ac6315bef8c07e6ed29ae5b92b85cffee3d0848
bww1 <bww1@hw.ac.uk>
2020-02-12 01:06:11 +0000

    Changes

--- uiAssets/content_registrationComplete.php
+++ uiAssets/content_registrationComplete.php
@@ -4,10 +4,10 @@ include_once dirname(__DIR__).'/required/config.php';
 function generateRegistrationComplete(){
     $html = <<<html
         <div class="container">
-            <div class="row">
+            <div class="row text-center">
                 <div class="text-center">
-                    <p>Thank you for registering for bubble!</p>
-                    <p>Please return to the <a href="index.php?action=login>Login Page</a> to login</p>
+                    <p class="text-center">Thank you for registering for bubble!</p>
+                    <p class="text-center">Please return to the <a href="index.php?action=login">Login Page</a> to login</p>
                 </div>
             </div>
         </div>   

6dc6ece8231e8bc495dafb5bee60c170a56145e4
bww1 <bww1@hw.ac.uk>
2020-02-12 01:45:04 +0000

    Changes

--- required/action_addRoom.php
+++ required/action_addRoom.php
@@ -10,14 +10,12 @@
 
     //BEGIN SESSION
     session_start();
-    $user_id = $_SESSION['user_id'];
     $hub_id = $_SESSION['hub_id'];
 
     //END SESSION
     session_write_close();
-    echo("$user_id  $hub_id  $roomName   $roomIcon");
-    $stmt = $db->prepare("INSERT INTO room_info (user_id, hub_id, room_name, room_icon) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("iisi", $user_id, $hub_id, $roomName, $roomIcon);
+    $stmt = $db->prepare("INSERT INTO room_info (hub_id, room_name, room_icon) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("isi", $hub_id, $roomName, $roomIcon);
     $stmt->execute();
     $result = $stmt->get_result();
     if ($result->num_rows > 0) {

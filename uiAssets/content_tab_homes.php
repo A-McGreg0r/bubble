4a291338a6c8d7eb3435bd6f14d5c3fdaa6c1086
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-02-26 05:43:47 +0000

    chart tweaks

--- uiAssets/content_tab_homes.php
+++ uiAssets/content_tab_homes.php
@@ -200,19 +200,19 @@ function generateHomeTab()
                                     <!--Carousel Container-->
                                     
                                     <div calss="border border-primary rounded m-2">
-                                      <!--change chart drop down-->
-                                    <select id="chartPicker" class="browser-default custom-select dropdown" onchange="chartSelect()">
-                                        <option selected="selected">Choose time period</option>
-                                        <option value="0">Year</option>
-                                        <option value="1">Month</option>
-                                        <option value="2">Day</option>
-                                    </select>
-                                    
-                                    
-                                     <canvas id="masterLineChart"></canvas>
-
+                                          <!--change chart drop down-->
+                                            <select id="chartPicker" class="browser-default custom-select dropdown" onchange="chartSelect()">
+                                                <option selected="selected">Choose time period</option>
+                                                <option value="0">Year</option>
+                                                <option value="1">Month</option>
+                                                <option value="2">Day</option>
+                                            </select>
+                                    <!--chart canvas-->        
+                                    <canvas id="masterLineChart"></canvas>
+    
                                     <script type="text/javascript">
-                                        // Supplied Datasets to display
+                                        //todo cahe where datas comeing from                                         
+                                        //Supplied Datasets to display
                                         //hourly 1 upto 24
                                         let data1 = { "labels": $DataLabelsEncoded,"label": "Expected Usage: ", "datasets": [{ "label": "avg", "data": $dataAvgEncoded, "backgroundColor": "rgba(101, 209, 159, 0.6)", "borderColor": "rgba(101, 209, 159,1)", "borderWidth": 1 },{ "label": "Power usage", "data": $dataPointsEncoded, "backgroundColor": "rgba(93, 176, 201, 0.6)", "borderColor": "rgba(0, 10, 130, .4)", "borderWidth": 1 }] };
                                         //days upto 31 days
@@ -224,21 +224,21 @@ function generateHomeTab()
                                         let jsonData = {data1,data2,data3};
                                         // Draw the initial chart
                                         let ctxL = $("#masterLineChart")[0].getContext('2d');
-                                        let masterLineChart = new Chart(ctxL, {
-                                            type: 'line',
-                                            data: data1,
-                                            options: {
-                                                scales: {
-                                                    yAxes: [{
-                                                        ticks: {
-                                                            beginAtZero: true
-                                                        }
-                                                    }]
+                                            let masterLineChart = new Chart(ctxL, {
+                                                type: 'line',
+                                                data: data1,
+                                                options: {
+                                                    scales: {
+                                                        yAxes: [{
+                                                            ticks: {
+                                                                beginAtZero: true
+                                                            }
+                                                        }]
+                                                    }
                                                 }
-                                            }
-                                        });
+                                            });
                                 
-                                    // Called on Click
+                                    // Called on Change
                                     $(document).ready(function(){
                                         $("select.dropdown").change(function(){
                                             let selectedChart = $(this).children("option:selected").val();

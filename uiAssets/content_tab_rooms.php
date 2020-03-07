70ac0bc783d5a4d474eb3f41f93c82f444794604
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-03-07 23:52:17 +0000

    starting button groups

--- uiAssets/content_tab_rooms.php
+++ uiAssets/content_tab_rooms.php
@@ -72,7 +72,7 @@ html;
                         <div class="d-flex flex-column">
                             <!-- Default switch -->
                             <div class="custom-control custom-switch">
-                                <form id="$room_id" submit="toggleRoom($room_id;)" method="POST" onclick="buttonGroup(this.id)">
+                                <form  submit="toggleRoom($room_id;)" method="POST" onclick="buttonGroup()">
                                     <input type="checkbox" class="custom-control-input" id="$room_name">
                                     <label class="custom-control-label" for="$room_name">off/on</label>
                                 </form>
@@ -81,10 +81,10 @@ html;
                     </div>
                 </div>
                 <script>
-                function buttonGroup(roomID) {
-                    let button = document.querySelector(roomID);
+                function buttonGroup() {
+                    let button = document.getSelection($room_id);
                         button.addEventListener("click", function(e) {
-                          alert("button: "+ roomID+ "clicked");
+                          alert("button: "+ roomID + "clicked");
                         });
                 }
                 </script>

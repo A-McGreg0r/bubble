f509b17c8507b4285b7d93699e2535a85996b9b7
Michael Linton <ml96@hw.ac.uk>
2020-03-12 23:29:46 +0000

    Update uiAssets/alert.php

--- uiAssets/alert.php
+++ uiAssets/alert.php
@@ -5,7 +5,7 @@ function alert($msg) {
     echo "<script type='text/javascript'>alert('$msg');</script>";
 }
 
-$command = escapeshellcmd('/home/pi/tmpsftp.py "1110123456" "1"');
+$command = escapeshellcmd('python /home/pi/tmpsftp.py "1110123456" "1"');
 $output = shell_exec($command);
 echo $output;
 ?>

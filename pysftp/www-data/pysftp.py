8cc35c87e28dbe5aa7bcfbcfd4380bc6e3b4e9d7
Michael Linton <ml96"hw.ac.uk>
2020-03-13 18:13:12 +0000

    Added enviroment

--- pysftp/www-data/pysftp.py
+++ pysftp/www-data/pysftp.py
@@ -1,17 +1,13 @@
 import sys, os
-File="device.json"
-Path="/var/www/"
-User="www-data/"
-filepath=Path+File
-userpath=User+Path
-os.system('cd '+userpath)
-def write_file(tmp):
-  file = open(tmp, 'w')
-  file.write('{\n')
-  file.write('\t"device": "'+sys.argv[1]+'",\n')
-  file.write('\t"state": "'+sys.argv[2]+'"\n')
-  file.write('}\n')
-  file.close()
+#filepath="device.json"
+#os.system('cd /var/www/pysftp/www-data/')
+#def write_file(tmp):
+#  file = open(tmp, 'w')
+#  file.write('{\n')
+#  file.write('\t"device": "'+sys.argv[1]+'",\n')
+#  file.write('\t"state": "'+sys.argv[2]+'"\n')
+#  file.write('}\n')
+#  file.close()
 
-write_file(filepath)
+#write_file(filepath)
 os.system('cat device.json | sshpass -p pi ssh pi@bubble-hub "cat > /home/pi/notify/device.json" &')

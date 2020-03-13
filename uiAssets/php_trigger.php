5edca2593bcb2b00556b935187823b22b5de2169
Michael Linton <ml96@hw.ac.uk>
2020-03-13 01:02:25 +0000

    Update uiAssets/php_trigger.php

--- uiAssets/php_trigger.php
+++ uiAssets/php_trigger.php
@@ -1,5 +1,4 @@
 <?php
-echo system('pwd');
-echo system('python');
-echo system('/home/pi/testEnv/bin/python /home/pi/tmpsftp.py "1110123456" "1"');
+echo passthru('pwd');
+echo passthru('/home/pi/testEnv/bin/python /home/pi/tmpsftp.py "1110123456" "1"');
 ?>

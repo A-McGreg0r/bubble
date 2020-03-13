2d3e8e768f23ad39b862052040cab71be478d911
Michael Linton <ml96@hw.ac.uk>
2020-03-13 00:17:59 +0000

    Update uiAssets/alert.php

--- uiAssets/alert.php
+++ uiAssets/alert.php
@@ -4,7 +4,6 @@ alert("Hello World");
 function alert($msg) {
     echo "<script type='text/javascript'>alert('$msg');</script>";
 }
-
-$out = shell_exec('python /home/pi/tmpsftp.py "1110123456" "1"');
+$out = shell_exec('/home/pi/testEnv/bin/python /home/pi/tmpsftp.py "1110123456" "1"');
 echo $out;
 ?>

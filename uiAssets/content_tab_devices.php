768edc2ad4a60d0f60af32a611e24e0e30efa6da
Rory Dobson <rmd5@hw.ac.uk>
2020-03-13 16:11:07 +0000

    Revert "Fix room and device style"
    This reverts commit 33922364377ac93a275f2f3a9047afdbf16c5da2

--- uiAssets/content_tab_devices.php
+++ uiAssets/content_tab_devices.php
@@ -2,9 +2,12 @@
 <?php
 include_once dirname(__DIR__).'/required/config.php';
 
+
+
 function generateDeviceTab(){
     $html = '';
-    
+
+
     $html .= <<<html
         <a href="index.php?action=adddevice">
             <div class="card mb-4 container">
@@ -35,16 +38,18 @@ html;
         $hub_id = $_SESSION['hub_id'];
         session_write_close();
 
-        $stmt = $db->prepare("SELECT * FROM device_info WHERE hub_id = ?");
+        $stmt = $db->prepare("SELECT * FROM device_info WHERE hub_id = ?");//todo oderby room id
         $stmt->bind_param("i", $hub_id);
         $stmt->execute();
         $result = $stmt->get_result();
 
         if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
+            while ($row = $result->fetch_assoc()) {
                 $device_id = $row['device_id'];
                 $device_name = $row['device_name'];
                 $device_type = $row['device_type'];
+                $device_room_id = $row['room_id'];
+                $deviceLocation = json_encode($device_room_id);
 
                 $stmt3 = $db->prepare("SELECT * FROM device_types WHERE type_id = ?");
                 $stmt3->bind_param("i", $device_type);
@@ -53,9 +58,30 @@ html;
                 $row3 = $result3->fetch_assoc();
                 $icon = $row3['type_icon'];
 
+                $stmt4 = $db->prepare("SELECT * FROM room_info WHERE room_id = ?");
+                $stmt4->bind_param("i", $device_room_id);
+                $stmt4->execute();
+
+                $result4 = $stmt4->get_result();
+                $row4 = $result4->fetch_assoc();
+                $room_type = $row4['room_name'];
+
+                //todo intagrate in to device page?
+                function deviceCat($device_type, $device_name)
+                {
+                    if ($device_type == "heating" || $device_type == "airCon") {
+                        $optionType = "<form class=\"range-field\" for=\"$device_name\"><input type=\"range\" min=\"0c\" max=\"40c\" /></form>";
+                        //todo add option for different temp measurements farnehight, celcus
+                    } else {
+                        $optionType = "<label class=\"custom-control-label\" for=\"$device_name\">off/on</label>";
+                    }
+                    return $optionType;
+                }
+
+
                 $html .= <<<html
                 <!-- Card -->
-                <div class="card mb-4 container grey-out">
+                <div class="card mb-4 container text-dark">
                     <!--Card image-->
                     <div class="view overlay">
                         <div class="mask rgba-white-slight"></div>
@@ -66,26 +92,41 @@ html;
                 
                         <!--Title-->      
                         <div class="d-flex flex-column">  
-                            <div class="row">
+                            <div class="flex-sm-row">
                                 $icon &nbsp; $device_name
-                            </div>
+                                 </div>
+                            <div class="flex-sm-row">
+                                room: &nbsp; $room_type
+                            </div>                                
+                            
                         </div>
+
                         
                         <div class="d-flex flex-column">
                             <!-- Default switch -->
                             <div class="custom-control custom-switch">
-                                <form onsubmit="toggleDevice($device_id;)" method="POST">
+                               <form action="#" method="POST">
                                     <input type="checkbox" class="custom-control-input" id="$device_name">
                                     <label class="custom-control-label" for="$device_name">off/on</label>
-                                </form>
-                            </div>  
-                        </div>
+                               </form>
+                               <script>
+                                 $('#$device_name').change(function() {
+                                    let check = $(this);
+                                 if (check.prop('checked') === true){
 
+                                     alert('ON $device_id');
+                                 }
+				 else {
+				     alert('OFF $device_id');
+				 }
+                                 });
+                               </script>
+                            </div>
+                        </div>
                     </div>
                 </div>
 html;
                 $stmt3->close();
-
             }
         }
         $stmt->close();
@@ -94,5 +135,4 @@ html;
     }
     return $html;
 }
-   
 ?>

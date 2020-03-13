df1128bfc35a1776fc245c7c01cf8d7425c35c35
rmd5 <rmd5@hw.ac.uk>
2020-03-13 15:54:20 +0000

    Card header fix

--- uiAssets/content_tab_homes.php
+++ uiAssets/content_tab_homes.php
@@ -112,7 +112,7 @@ function generateHomeTab()
         
                         <!-- Card body -->
                         <div id="collapse$hub_id" class="collapse show" role="tabpanel" aria-labelledby="heading$hub_id" data-parent="#accordionEx194">
-                            <div class="card-body pt-0 justify-content-center " style="max-width:100%">
+                            <div class="card-body pt-0 justify-content-center " style="max-width:95%">
                             <!--todo change 3 donuts to carousels-->
                                 <div class="flex-sm-row ">    
                                 
@@ -124,7 +124,7 @@ function generateHomeTab()
                                         
                                             <!--Donut 1-->
                                                   <div class="carousel-item active">                           
-                                                        <div class="col border border-primary rounded m-2" style="max-width:100%">
+                                                        <div class="col border border-primary rounded m-2" style="max-width:95%">
                                                             <h4 class="text-centre text-dark centre-text">Daily</h4>
                                                             
                                                             <canvas style="max-width:50% min-width:30%" id="heatingUsage"></canvas>
@@ -184,7 +184,7 @@ function generateHomeTab()
                                             
                                             <!--Donut 3-->             
                                                   <div class="carousel-item">
-                                                        <div class="col border border-primary rounded m-2" style="max-width:100%">
+                                                        <div class="col border border-primary rounded m-2" style="max-width:95%">
                                                             <h4 class="text-centre text-dark centre-text">Variance</h4>
                                                             <canvas style="max-width:50% min-width:30%" id="heatingUsage2"></canvas>
                                                             <script>

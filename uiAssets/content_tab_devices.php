93d25b2e075fb45a90769e6c34b4c5f4dc8822c9
Michael Linton <ml96@hw.ac.uk>
2020-03-13 15:11:47 +0000

    Update uiAssets/content_tab_devices.php

--- uiAssets/content_tab_devices.php
+++ uiAssets/content_tab_devices.php
@@ -109,9 +109,15 @@ html;
                                     <input type="checkbox" class="custom-control-input" id="$device_name">
                                     <label class="custom-control-label" for="$device_name">off/on</label>
                                </form>
+                               <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
                                <script>
                                 $('#$device_name').change(function() {
                                     let check = $(this);
+                                    $(document).ready(function(){
+                                        $("button").click(function(){
+                                            $("#div1").load("alert.php");
+                                        });
+                                    });
                                     if (check.prop('checked') === true){
                                         alert('ON $device_id');
                                     }

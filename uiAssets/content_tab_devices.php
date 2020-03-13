d496dd5fbce009cfb0cfe669c34b9b91328bc460
Michael Linton <ml96@hw.ac.uk>
2020-03-13 15:05:59 +0000

    AJAX

--- uiAssets/content_tab_devices.php
+++ uiAssets/content_tab_devices.php
@@ -109,15 +109,24 @@ html;
                                     <input type="checkbox" class="custom-control-input" id="$device_name">
                                     <label class="custom-control-label" for="$device_name">off/on</label>
                                </form>
+                               <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
                                <script>
                                 $('#$device_name').change(function() {
                                     let check = $(this);
-                                 if (check.prop('checked') === true){
-                                     alert('ON $device_id');
-                                 }
-				                 else {
-				                     alert('OFF $device_id');
-				                 }  
+                                    if (check.prop('checked') === true){
+                                        $.post("demo_test_post.asp",
+                                        {
+                                            name: "Donald Duck",
+                                            city: "Duckburg"
+                                        },
+                                        function(data,status){
+                                            alert("Data: " + data + "\nStatus: " + status);
+                                        });
+                                        alert('ON $device_id');
+                                    }
+				                    else {
+				                        alert('OFF $device_id');
+				                    }  
                                  });
                                </script>
                             </div>

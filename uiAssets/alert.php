c9bc82bd6d363050e5ec5737f626e4693e8ce3d4
Michael Linton <ml96@hw.ac.uk>
2020-03-13 00:24:04 +0000

    Update uiAssets/alert.php, uiAssets/charts/php_trigger.php files

--- uiAssets/alert.php
+++ uiAssets/alert.php
@@ -1,9 +1,4 @@
 <?php
-alert("Hello World");
-
-function alert($msg) {
-    echo "<script type='text/javascript'>alert('$msg');</script>";
-}
 $out = shell_exec('/home/pi/testEnv/bin/python /home/pi/tmpsftp.py "1110123456" "1"');
 echo $out;
 ?>
---
+++ uiAssets/charts/php_trigger.php
@@ -0,0 +1,3 @@
+<?php
+exec('php -f alert.php');
+?>

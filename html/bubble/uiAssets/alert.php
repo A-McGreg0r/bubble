a8436b87f333254e88b06e86bcff050d580e3f21
Michael Linton <ml96"hw.ac.uk>
2020-03-16 16:02:52 +0000

    Update alert.php

--- html/bubble/uiAssets/alert.php
+++ html/bubble/uiAssets/alert.php
@@ -1,4 +1,4 @@
 <?php
-$out = shell_exec('/./var/www/.local/bin/python2.7 /var/www/.local/www-data/pysftp.py');
-echo $out;
+$out = exec('/var/www/.local/bin/python2.7 /var/www/.local/www-data/pysftp.py 1357 1', $out);
+var_dump($out);
 ?>

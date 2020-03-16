00fb11acce3ad89b955ee7d4b568f11fccf0f28f
bww1 <bww1@hw.ac.uk>
2020-03-16 15:30:44 +0000

    Change to jquery ajax call

--- html/bubble/js/bubble.js
+++ html/bubble/js/bubble.js
@@ -18,21 +18,19 @@ function toggleRoom(room_id){
 
 
 function toggleDevice(device_id) {
-    alert("Hello! I am an alert box!!");
-    let url = "/required/action_device.php?type=device&id=" + device_id;
-    let request = new XMLHttpRequest();
-    request.open('POST', url, true);
-    request.onload = function () {
-
-        console.log(request.responseText);
-    };
-
-    request.onerror = function () {
-
-    };
-
-    request.send(new FormData(event.target));
-    event.preventDefault();
+    let url = "required/action_device.php?type=device&id=" + device_id;
+    
+    $.ajax({
+        type:'POST',
+        url: url,
+        data:{ type: "device", id: device_id},
+        success:function(data){
+            alert(data);
+        },
+        error: function(data){
+            alert("error!");
+        }
+    });
 }
 
 

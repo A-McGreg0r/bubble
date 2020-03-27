d771119a129cb24f7f7f5e556453706626edd447
rmd5 <rmd5@hw.ac.uk>
2020-03-27 14:16:46 +0000

    push subscription and notifications

--- html/bubble/required/webpush/authentication_vapid.php
+++ html/bubble/required/webpush/authentication_vapid.php
@@ -8,9 +8,10 @@ $auth = [
     //'GCM' => 'MY_GCM_API_KEY', // deprecated and optional, it's here only for compatibility reasons
     'VAPID' => [
         'subject' => 'https://bubble.rorydobson.com', // can be a mailto: or your website address
-        'publicKey' => 'BP7vLpKSxbsOv4085FziNHN8nZEQYYPvgLqgOOghiFYVdsiRXAbqZnzCuLrCKWP6S3gsM_4TzkFO5Fk5yv7D330', // (recommended) uncompressed public key P-256 encoded in Base64-URL
-        'privateKey' => '3gsDjggr_P-sSnyQoel8xaNozZOPLO1nUzuwe4KbwjE', // (recommended) in fact the secret multiplier of the private key encoded in Base64-URL
-        'pemFile' => 'private_key.pem', // if you have a PEM file and can link to it on your filesystem
+        'publicKey' => file_get_contents(__DIR__ . '/../webpush/public_key.txt'), // (recommended) uncompressed public key P-256 encoded in Base64-URL
+        'privateKey' => file_get_contents(__DIR__ . '/../webpush/private_key.txt'), // (recommended) in fact the secret multiplier of the private key encoded in Base64-URL
+        'pemFile' => '/../webpush/private_key.pem', // if you have a PEM file and can link to it on your filesystem
+        'pem' => file_get_contents(__DIR__ . '/../webpush/private_key.pem'), // if you have a PEM file and want to hardcode its content
     ],
 ];
 

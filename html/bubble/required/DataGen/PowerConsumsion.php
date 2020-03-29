354d1d906dce953268424ba2ce44f0f251dbc8fb
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-03-29 18:40:23 +0100

    updated algorithm for generating consumed power data added more modularaty to it

--- html/bubble/required/DataGen/PowerConsumsion.php
+++ html/bubble/required/DataGen/PowerConsumsion.php
@@ -2,36 +2,43 @@
 $currentDate= 0;
 
 $timeOfDay=0;
-$workStart =8;//8am
-$workEnd = 17;//5pm
-$workinghours=array( $workStart , $workEnd);
-$daysInWeek = array(true,true,true,true,true,false,false);//true == at work false not at work
 
 
-$Traveltime=1;
 
-$count=1;
 
-while($count <=7){
 
-    if ($daysInWeek[$count]==true) {
-        //get hours awake first
-        if($workinghours[1] > $timeOfDay && $workinghours[2] < $timeOfDay ){
-            lowConsumption($timeOfDay);
-            $count++;
-        }else{
-            highConsumption($timeOfDay);
+generatesConsumptionData(8,17,1);
+
+
+function generatesConsumptionData($workStart, $workEnd, $travleTime) {
+
+    $count=1;
+
+
+    $workinghours=array( $workStart , $workEnd);
+    $daysInWeek = array(true,true,true,true,true,false,false);//true == at work false not at work
+
+    while($count <=7){
+
+        if ($daysInWeek[$count]==true) {
+            //get hours awake first
+
+            if($workinghours[1] > $timeOfDay && $workinghours[2] < $timeOfDay ){
+                lowConsumption($timeOfDay);
+                $count++;
+            }else{
+                highConsumption($timeOfDay);
+            }
         }
-
-    }
-
-    if ($daysInWeek[$count]==false){
-        //if awake
+        if ($daysInWeek[$count]==false){
+            //if awake
             //call highConsumption high else call lowConsumption
 
-        $count++;
+            $count++;
+        }
     }
 }
+
 
 function highConsumption($time){
     $consumedPower=0;

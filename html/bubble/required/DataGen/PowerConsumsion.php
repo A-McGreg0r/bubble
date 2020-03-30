2d6849ed08ef7ba2892e1f5a0926d8a92c23d5de
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-03-30 21:00:46 +0100

    refactored broke main function down in to smaller components

--- html/bubble/required/DataGen/PowerConsumsion.php
+++ html/bubble/required/DataGen/PowerConsumsion.php
@@ -10,7 +10,7 @@ $stmt->execute();
 $result = $stmt->get_result();
 if ($result->num_rows >= 1) {
     $all = $result->fetch_all(MYSQLI_ASSOC);
-    foreach($all as $row){
+    foreach ($all as $row) {
         $energy_used = 0;
         $maxConsumption = 0;
 
@@ -20,7 +20,7 @@ if ($result->num_rows >= 1) {
         $result5 = $stmt5->get_result();
         if ($result5->num_rows >= 1) {
             $all5 = $result5->fetch_all(MYSQLI_ASSOC);
-            foreach($all5 as $row5){
+            foreach ($all5 as $row5) {
 
                 $stmt4 = $db->prepare("SELECT * FROM device_types WHERE type_id = ?");
                 $stmt4->bind_param("i", $row5['device_type']);
@@ -28,7 +28,7 @@ if ($result->num_rows >= 1) {
                 $result4 = $stmt4->get_result();
                 if ($result4->num_rows >= 1) {
                     $all4 = $result4->fetch_all(MYSQLI_ASSOC);
-                    foreach($all4 as $row4){
+                    foreach ($all4 as $row4) {
                         $maxConsumption = $energy_used + $row4['energy_usage'];
                         $energy_usage = $row4['energy_usage'];
                     }
@@ -37,87 +37,114 @@ if ($result->num_rows >= 1) {
         }
     }
 }
-generatesConsumptionData(5,800,1700,100,$maxConsumption);
+generatesConsumptionData(5, 800, 1700, 100, $maxConsumption);
 
-function generatesConsumptionData($workingDays,$workStart, $workEnd, $travelTime,$maxConsumption) {
+function generatesConsumptionData($workingDays, $workStart, $workEnd, $travelTime, $maxConsumption)
+{
     echo "Func Called \n";
-    $daysInWeek=7;//number of days in the week
-    $dayCount=1;//start point for days in week
+    $daysInWeek = 7;//number of days in the week
+    $dayCount = 1;//start point for days in week
 
-    $hoursInDay=2400;//number of hours in a day
-    $sleepingHoursStart=2200;
-    $sleepingHoursEnd=600;
-    $weeklyData=array($daylayData=array());
-    $consumed=0;
+    $hoursInDay = 2400;//number of hours in a day
+    $sleepingHoursStart = 2200;
+    $sleepingHoursEnd = 600;
+    $weeklyData = array();
+    $consumed = 0;
 
-    while($dayCount <=$daysInWeek){
-        $TimeOfDay=100;//start point of hours loop
-        while($dayCount<=$workingDays){
-            echo "\n Working-Day :$dayCount \n";
-            $TimeOfDay=100;//start point of hours loop
+    while ($dayCount <= $daysInWeek) {
+        $TimeOfDay = 100;//start point of hours loop
+        while ($dayCount <= $workingDays) {
+            GenWeekDay($maxConsumption, $dayCount, $workStart, $workEnd, $travelTime, $sleepingHoursStart, $sleepingHoursEnd, $TimeOfDay);
             $dayCount++;
-
-            while ($TimeOfDay <= $hoursInDay){
-                if($TimeOfDay==$workStart){echo"\nStartingWork\n";}
-
-                if($TimeOfDay > $workStart-$travelTime && $TimeOfDay < $workEnd+$travelTime){
-                    $consumed =lowConsumption($maxConsumption);
-                    echo "\tWorking-Hour: $TimeOfDay\tPowerCounsumed: $consumed\n";
-                    array_push($daylayData,$consumed);
-
-                    //bug
-                }else if ($TimeOfDay < $sleepingHoursStart && $TimeOfDay > $sleepingHoursEnd){
-                    $consumed =$consumed+highConsumption($maxConsumption);
-                    echo "\tHOME-Hour: $TimeOfDay\t\tPowerCounsumed: $consumed\n";
-                    array_push($daylayData,$consumed);
-                }else{
-                    $consumed =lowConsumption($maxConsumption);
-                    echo "\tSlepping-Hour: $TimeOfDay\tPowerCounsumed: $consumed\n";
-                    array_push($daylayData,$consumed);
-                }
-
-                $TimeOfDay=+$TimeOfDay+100;
-            }
-            $consumed=0;
         }
-        while($dayCount <= $daysInWeek){
-            echo "WeekendDay :$dayCount\n";
-            $TimeOfDay=100;//start point of hours loop
-            while ($TimeOfDay < $hoursInDay){
-
-                $TimeOfDay=+$TimeOfDay+100;
-                if ($TimeOfDay < $sleepingHoursStart && $TimeOfDay > $sleepingHoursEnd){
-                    $consumed =highConsumption($maxConsumption);
-                    echo "\tHOME-Hour: $TimeOfDay\t\tPowerCounsumed: $consumed\n";
-                    array_push($daylayData,$consumed);
-                }else{
-                    $consumed =lowConsumption($maxConsumption);
-                    echo "\tSlepping-Hour: $TimeOfDay\tPowerCounsumed: $consumed\n";
-                    array_push($daylayData,$consumed);
-                }
-            }
+        while ($dayCount <= $daysInWeek) {
+            GenWeekendDay($maxConsumption, $dayCount, $sleepingHoursStart, $sleepingHoursEnd, $hoursInDay);
             $dayCount++;
-            array_push($weeklyData,$daylayData);
         }
     }
-    print_r($weeklyData);
+    //print_r($weeklyData);
+}
+
+function GenWeekDay($maxConsumption, $dayCount, $workStart, $workEnd, $travelTime, $sleepingHoursStart, $sleepingHoursEnd, $TimeOfDay)
+{
+    echo "\nWorking-Day :$dayCount \n";
+    $data = array();
+    $consumed = 0;
+    $workingHours = $workEnd - $workStart;
+    $hoursInDay = 2400;
+    echo "Working hours: $workingHours \n";
+    while ($TimeOfDay <= $hoursInDay) {
+        if ($TimeOfDay == $workStart) {
+            echo "\nStartingWork\n";
+        }
+
+        if ($TimeOfDay > $workStart - $travelTime && $TimeOfDay < $workEnd + $travelTime) {
+            $consumed = lowConsumption($maxConsumption);
+            echo "\tWorking-Hour: $TimeOfDay\tPowerCounsumed: $consumed\n";
+            array_push($data, $consumed);
+
+
+        } else if ($TimeOfDay < $sleepingHoursStart && $TimeOfDay > $sleepingHoursEnd) {
+
+            $consumed = $consumed + highConsumption($maxConsumption);
+            echo "\tHOME-Hour: $TimeOfDay\t\tPowerCounsumed: $consumed\n";
+            array_push($data, $consumed);
+        } else {
+            $consumed = lowConsumption($maxConsumption);
+            echo "\tSlepping-Hour: $TimeOfDay\tPowerCounsumed: $consumed\n";
+            array_push($data, $consumed);
+        }
+
+        $TimeOfDay = +$TimeOfDay + 100;
+
+    }
+    return $data;
+}
+
+function GenWeekendDay($maxConsumption, $dayCount, $sleepingHoursStart, $sleepingHoursEnd, $hoursInDay)
+{
+    echo "func test\n";
+    $consumed = 0;
+    $data = array();
+    echo "WeekendDay :$dayCount\n";
+    $TimeOfDay = 100;//start point of hours loop
+    while ($TimeOfDay < $hoursInDay) {
+        $TimeOfDay = +$TimeOfDay + 100;
+        if ($TimeOfDay < $sleepingHoursStart && $TimeOfDay > $sleepingHoursEnd) {
+            $consumed = highConsumption($maxConsumption);
+            echo "\tHOME-Hour: $TimeOfDay\t\tPowerCounsumed: $consumed\n";
+            array_push($data, $consumed);
+        } else {
+            $consumed = lowConsumption($maxConsumption);
+            echo "\tSlepping-Hour: $TimeOfDay\tPowerCounsumed: $consumed\n";
+            array_push($data, $consumed);
+        }
+    }
+    return $data;
+    //post to DB
 }
 
 
-function highConsumption($maxConsumption){
-    $numerator = rand(2,10);
-    $denominator = rand(3,10);
+function highConsumption($maxConsumption)
+{
+    $energy_used = 0;
+    $energy_usage = 0;
+
+    $numerator = rand(2, 10);
+    $denominator = rand(3, 10);
     while ($numerator > $denominator) {
-        $numerator = rand(2,10);
-        $denominator = rand(3,10);
+        $numerator = rand(2, 10);
+        $denominator = rand(3, 10);
     }
 
-    $idle_energy = rand(0,$maxConsumption/3);
+    $idle_energy = rand(0, $maxConsumption / 3);
     $energy_usage = $energy_usage / $denominator * $numerator;
     $energy_used = $energy_used + $idle_energy;
-    return energy_used;
+    return $energy_used;
 }
-function lowConsumption($maxConsumption){
-    $energy_used = rand(0,$maxConsumption/3);
+
+function lowConsumption($maxConsumption)
+{
+    $energy_used = rand(0, $maxConsumption / 3);
     return $energy_used;
 }

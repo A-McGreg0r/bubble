ea391dd05b109bceea14bad06ecf7d441a79c723
rmd5 <rmd5@hw.ac.uk>
2020-03-30 13:56:27 +0100

    Fix day to month transfer for new database setup

--- html/bubble/required/DataGen/transfer_day_to_month.php
+++ html/bubble/required/DataGen/transfer_day_to_month.php
@@ -15,26 +15,33 @@ if ($result->num_rows >= 1) {
 
         $hub_id = $row['hub_id'];
 
-        $stmt2 = $db->prepare("SELECT * FROM annual_data");
+        $stmt2 = $db->prepare("SELECT * FROM monthly_data");
         $stmt2->execute();
         $result2 = $stmt2->get_result();
 
-        if ($result2->num_rows === 0){
-            $stmt3 = $db->prepare("INSERT INTO annual_data (hub_id) VALUES (?)");
-            $stmt3->bind_param("i", $hub_id);
-            $stmt3->execute();
-        }
+        $monthly_energy = 0;
 
-        $stmt4 = $db->prepare("SELECT * FROM monthly_data");
+        $stmt4 = $db->prepare("SELECT * FROM daily_data WHERE hub_id = ?");
+        $stmt4->bind_param("i", $hub_id);
         $stmt4->execute();
         $result4 = $stmt4->get_result();
-        extract($result4->fetch_assoc());
-
-        $monthly_energy = $day_01 + $day_02 + $day_03 + $day_04 + $day_05 + $day_06 + $day_07 + $day_08 + $day_09 + $day_10 + $day_11 + $day_12 + $day_13 + $day_14 + $day_15 + $day_16 + $day_17 + $day_18 + $day_19 + $day_20 + $day_21 + $day_22 + $day_23 + $day_24 + $day_25 + $day_26 + $day_27 + $day_28 + $day_29 + $day_30 + $day_31;
-    
-        $stmt5 = $db->prepare("UPDATE annual_data SET month_$month = ? WHERE hub_id = ?");
-        $stmt5->bind_param("ii", $monthly_energy, $hub_id);
-        $stmt5->execute();
+        $num_rows = $result4->num_rows;
+        if ($num_rows >= 1) {
+            $all4 = $result4->fetch_all(MYSQLI_ASSOC);
+            foreach($all4 as $row4){
+                $monthly_energy = $monthly_energy + $row4['energy_usage'];
+                if ($num_rows >= 13){
+                    $num_rows = $num_rows - 1;
+                    $stmt6 = $db->prepare("DELETE FROM daily_data WHERE entry_id = ?");
+                    $stmt6->bind_param("i", $row4['entry_id']);
+                    $stmt6->execute();
+                }
+            }
+                
+            $stmt5 = $db->prepare("INSERT INTO monthly_data (hub_id, entry_year, entry_month, energy_usage) VALUES (?, ?, ?, ?)");
+            $stmt5->bind_param("iiii", $hub_id, $year, $month, $monthly_energy);
+            $stmt5->execute();
+        }
 
         $stmt2->close();
         $stmt3->close();

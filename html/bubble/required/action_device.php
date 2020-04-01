8c4e7412de9bbf782aefa235aeaa3ecd11d1d789
bww1 <bww1@hw.ac.uk>
2020-04-01 21:28:24 +0100

    Work on jquery changing background

--- html/bubble/required/action_device.php
+++ html/bubble/required/action_device.php
@@ -1,7 +1,9 @@
 <?php
 ////////////////////////////////////// DEVICE CONTROL SCRIPT/////////////////////////////////////////////
 include_once 'config.php';
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 //GRAB TYPE FROM POST
 $type = "device";
 if(isset($_POST['type'])) $status = $_POST['type'];

6077f98f9d08df5885d9a2abf9cb1c421c3ed828
Michael Linton <ml96@hw.ac.uk>
2020-04-02 05:00:26 +0000

    Update averagePowerGeneration.php

--- html/bubble/required/DataGen/averagePowerGeneration.php
+++ html/bubble/required/DataGen/averagePowerGeneration.php
@@ -15,103 +15,31 @@ function _main($Y, $anual_power_gen) {
     function season_inc($case, $S) {
         switch ($case) {
             case 0:
-                $name = 'winter';
+                //Winter
                 $percentage = 12.5;
                 $sum = $S[0];
-                echo "$name<br>";
-                echo "$percentage<br>";
-                echo "$sum<br><br>";
                 break;
             case 1:
-                $name = 'spring';
+                //Spring
                 $percentage = 25.0;
                 $sum = $S[1];
-                echo "$name<br>";
-                echo "$percentage<br>";
-                echo "$sum<br><br>";
                 break;
             case 2:
-                $name = 'summer';
+                //Summer
                 $percentage = 37.5;
                 $sum = $S[2];
-                echo "$name<br>";
-                echo "$percentage<br>";
-                echo "$sum<br><br>";
                 break;
             case 3:
-                $name = 'fall';
+                //Fall
                 $percentage = 25.0;
                 $sum = $S[3];
-                echo "$name<br>";
-                echo "$percentage<br>";
-                echo "$sum<br><br>";    
                 break;
             default:
                 echo "Invalid Season.";
         }
-        $midpoint=round(($sum/2),0);
-        for ($i = 1; $i <= $midpoint; $i++) {$sum_total = $sum_total + $i;}
-        $inc = $percentage / $sum_total;
-        return $inc;
-    }
-    
-    function print_daily_season_inc($case, $S, $I, $P) {
-        switch ($case) {
-            case 0:
-                $inc = $I[0];
-                $sum = $S[0];
-                $mid=round(($sum/2),0);
-                $pointer=$mid;
-                for ($d=1; $d <= $sum; $d++) {
-                    if ($d <= $mid) {
-                        $percentage = $pointer*$inc/100;
-                        $pointer--;
-                    }
-                    else {$percentage = ($d-$mid)*$inc/100;}
-                    echo "day [";echo $d;echo "]::";
-                    echo $percentage;echo "%    ";
-                    echo $percentage*$P;echo " Watts<br>";
-                }
-                break;
-            case 1:
-                $inc = $I[1];
-                $sum = $S[1];
-                for ($d=1; $d <= $sum; $d++) {
-                    echo "day [";echo $d;echo "]::";
-                    echo $d*$inc/100;echo "%    ";
-                    echo round(($d*$inc/100)*$P, 2);echo " Watts<br>";
-                }
-                break;
-            case 2:
-                $inc = $I[2];
-                $sum = $S[2];
-                $mid=round(($sum/2),0);
-                $pointer=$mid;
-                for ($d=1; $d <= $sum; $d++) {
-                    if ($d > $mid) {
-                        $percentage = $pointer*$inc/100;
-                        $pointer--;
-                    }
-                    else {$percentage = $d*$inc/100;}
-                    echo "day [";echo $d;echo "]::";
-                    echo $percentage;echo "%    ";
-                    echo $percentage*$P;echo " Watts<br>";
-                }
-                break;
-            case 3:
-                $inc = $I[3];
-                $sum = $S[3];
-                $pointer = $sum;
-                for ($d=1; $d <= $sum; $d++) {
-                    echo "day [";echo $d;echo "]::";
-                    echo $pointer*$inc/100;echo "%    ";
-                    echo round(($pointer*$inc/100)*$P, 2);echo " Watts<br>";
-                    $pointer--;
-                }
-                break;
-            default:
-                echo "Invalid Season.";
-            }
+        $mid_trunc=round(($sum/2),0);
+        for ($i = 1; $i <= $mid_trunc; $i++) {$sum_total = $sum_total + $i;}
+        return ($percentage / $sum_total);
     }
 
     function season_calc($case, $S, $I, $P, $Y) {
@@ -130,29 +58,42 @@ function _main($Y, $anual_power_gen) {
                         $pointer--;
                     }
                     else {$percentage = ($d-$mid)*$inc/100;}
-                    echo "day [";echo $d;echo "]::";
                     //echo $percentage;echo "%    "
                     //INSERT INTO table VALUES ($d ,$percentage*$P)
                     $diff = $d-$previous_max_days;
                     if ($diff < $max_days) {
                         //INSERT INTO table VALUES ($d ,$percentage*$P)
-                        echo $percentage*$P;echo " W<br>";
+                        $pow=$percentage*$P;
                     }
                     else {
                         $previous_max_days = $d;
                         $m++;
                         $max_days=cal_days_in_month(CAL_GREGORIAN, $m, $Y);
                         //echo date("m");echo " <br>";
-                        echo $percentage*$P;echo " W<br>";
+                        $watts=$percentage*$P;
                     }
                     //INSERT INTO table VALUES ($d ,$percentage*$P)
                 }
                 break;
             case 1:
                 for ($d=1; $d <= $sum; $d++) {
-                    echo "day [";echo $d;echo "]::";
                     //echo $d*$inc/100;echo "%    ";
-                    echo round(($d*$inc/100)*$P, 2);echo " W<br>";
+
+                    //echo $percentage;echo "%    "
+                    //INSERT INTO table VALUES ($d ,$percentage*$P)
+                    $diff = $d-$previous_max_days;
+                    if ($diff < $max_days) {
+                        //INSERT INTO table VALUES ( ($d-$previous_max_days) ,$percentage*$P)
+                        $watts = round($percentage*$P, 2);
+                    }
+                    else {
+                        $previous_max_days = $d;
+                        $m++;
+                        $max_days=cal_days_in_month(CAL_GREGORIAN, $m, $Y);
+                        //echo date("m");echo " <br>";
+                        $watts = round($percentage*$P, 2);
+                    }
+                    //INSERT INTO table VALUES ($d ,$percentage*$P)
                 }
                 break;
             case 2:
@@ -163,18 +104,45 @@ function _main($Y, $anual_power_gen) {
                         $pointer--;
                     }
                     else {$percentage = $d*$inc/100;}
-                    echo "day [";echo $d;echo "]::";
-                    //echo $percentage;echo "%    ";
-                    echo $percentage*$P;echo " W<br>";
+                    //echo $percentage;echo "%    "
+                    //INSERT INTO table VALUES ($d ,$percentage*$P)
+                    $diff = $d-$previous_max_days;
+                    if ($diff < $max_days) {
+                        //INSERT INTO table VALUES ( ($d-$previous_max_days) ,$percentage*$P)
+                        $watts = round($percentage*$P, 2);
+                    }
+                    else {
+                        $previous_max_days = $d;
+                        $m++;
+                        $max_days=cal_days_in_month(CAL_GREGORIAN, $m, $Y);
+                        //echo date("m");echo " <br>";
+                        $watts = round($percentage*$P, 2);
+                    }
+                    //INSERT INTO table VALUES ($d ,$percentage*$P)
                 }
                 break;
             case 3:
                 $pointer=$sum;
                 for ($d=1; $d <= $sum; $d++) {
-                    echo "day [";echo $d;echo "]::";
                     //echo $pointer*$inc/100;echo "%    ";
-                    echo round(($pointer*$inc/100)*$P, 2);echo " W<br>";
+                    
                     $pointer--;
+
+                    //echo $percentage;echo "%    "
+                    //INSERT INTO table VALUES ($d ,$percentage*$P)
+                    $diff = $d-$previous_max_days;
+                    if ($diff < $max_days) {
+                        //INSERT INTO table VALUES ( ($d-$previous_max_days) ,$percentage*$P)
+                        $watts = round(($pointer*$inc/100)*$P, 2);
+                    }
+                    else {
+                        $previous_max_days = $d;
+                        $m++;
+                        $max_days=cal_days_in_month(CAL_GREGORIAN, $m, $Y);
+                        //echo date("m");echo " <br>";
+                        $watts = round(($pointer*$inc/100)*$P, 2);
+                    }
+                    //INSERT INTO table VALUES ($d ,$percentage*$P)
                 }
                 break;
             default:
@@ -189,7 +157,6 @@ function _main($Y, $anual_power_gen) {
     for ($i=0;$i<4;$i++){
         array_push($inc, season_inc($i, $S));
         season_calc($i, $S, $inc, $P, $Y);
-        echo "<br><br>";
     }
     
 }

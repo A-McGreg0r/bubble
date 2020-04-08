325eb72092138ec2b0927cefb2ef84d1d91cd6c8
rmd5 <rmd5@hw.ac.uk>
2020-04-08 19:28:56 +0100

    Updates

--- html/bubble/required/email/budget_email.php
+++ html/bubble/required/email/budget_email.php
@@ -38,6 +38,7 @@ if ($result->num_rows >= 1) {
             }
 
             $total_spent = $energy_used / 1000 * $energy_cost;
+            $total_spent_round = number_format($total_spent,2);
 
             $allow_emails = $row['allow_emails'];
             if($allow_emails == "Yes"){
@@ -45,33 +46,35 @@ if ($result->num_rows >= 1) {
                 $email = $row['email'];
                 $budget = $row['budget'];
 
-                $mail = new PHPMailer;
-                $mail->isSMTP();
-                $mail->SMTPDebug = 2;
-                $mail->Host = 'smtp.gmail.com';
-                $mail->Port = 587;
-                $mail->SMTPAuth = true;
-                $mail->Username = 'bubblehome.care@gmail.com';
-                $mail->Password = 'TeamBubble6!';
-                $mail->setFrom('bubblehome.care@gmail.com', 'Bubble');
-                $mail->addAddress($email, $first_name);
-                $mail->Subject = 'Nearing budget limit';
-                $mail->isHTML(true);
-                $mail->AddEmbeddedImage('img/favicon.png', 'logo');
+                if (($budget * 0.1) >= ($budget - $total_spent)){
+                    $mail = new PHPMailer;
+                    $mail->isSMTP();
+                    $mail->SMTPDebug = 2;
+                    $mail->Host = 'smtp.gmail.com';
+                    $mail->Port = 587;
+                    $mail->SMTPAuth = true;
+                    $mail->Username = 'bubblehome.care@gmail.com';
+                    $mail->Password = 'TeamBubble6!';
+                    $mail->setFrom('bubblehome.care@gmail.com', 'Bubble');
+                    $mail->addAddress($email, $first_name);
+                    $mail->Subject = 'Nearing budget limit';
+                    $mail->isHTML(true);
+                    $mail->AddEmbeddedImage('img/favicon.png', 'logo');
 
-                $logo = "<img src='cid:logo' style='width:100px'>";
-                $email_welcome = "<h3 style='font-weight:400'>Dear $first_name,<h3>";
-                $email_body = "<h4 style='font-weight:400'>You are nearing your set budget of &#163;$budget per month!<br>You have currently spent &#163;$total_spent.<br>To reduce the amount you spend, please consider turning off all electronic devices when not in use, or have heaters or air conditioning units on at a lower setting.</h4>";
-                $email_signoff = "<h4 style='font-weight:400'>Kind regards,<br>The Bubble Team</h4>";
-                $email_signature = "Jamie Rice - Organisational Manager <br> Bruce Wilson - Technical Manager <br> Rory Dobson - Liaison Officer <br> Andrew MacGregor - Web Developer <br> Michael Linton - Software Engineer <br> Mark Kostryckyj - Software Developer";
-                $email_content = "<div style='font-family:arial'>$email_welcome $email_body $email_signoff</div> <hr><div style='text-align:center'> $logo <br> $email_signature</div>";
+                    $logo = "<img src='cid:logo' style='width:100px'>";
+                    $email_welcome = "<h3 style='font-weight:400'>Dear $first_name,<h3>";
+                    $email_body = "<h4 style='font-weight:400'>You are nearing your set budget of &#163;$budget per month!<br>You have currently spent &#163;$total_spent_round.<br><br>To reduce the amount you spend, please consider turning off all electronic devices when not in use, or have heaters or air conditioning units on at a lower setting.</h4>";
+                    $email_signoff = "<h4 style='font-weight:400'>Kind regards,<br>The Bubble Team</h4>";
+                    $email_signature = "Jamie Rice - Organisational Manager <br> Bruce Wilson - Technical Manager <br> Rory Dobson - Liaison Officer <br> Andrew MacGregor - Web Developer <br> Michael Linton - Software Engineer <br> Mark Kostryckyj - Software Developer";
+                    $email_content = "<div style='font-family:arial'>$email_welcome $email_body $email_signoff</div> <hr><div style='text-align:center'> $logo <br> $email_signature</div>";
 
-                $mail->Body = (string) $email_content;
-                //$mail->addAttachment('test.txt');
-                if (!$mail->send()) {
-                    echo 'Mailer Error: ' . $mail->ErrorInfo;
-                } else {
-                    echo "Email sent to $email\n";
+                    $mail->Body = (string) $email_content;
+                    //$mail->addAttachment('test.txt');
+                    if (!$mail->send()) {
+                        echo 'Mailer Error: ' . $mail->ErrorInfo;
+                    } else {
+                        echo "Email sent to $email\n";
+                    }
                 }
             }
         }
--- html/bubble/required/email/email_at_8.php
+++ html/bubble/required/email/email_at_8.php
@@ -65,9 +65,9 @@ if ($result->num_rows >= 1) {
                 $email_welcome = "<h3 style='font-weight:400'>Dear $first_name,<h3>";
                 $email_body = '';
                 if ($budget < $total_spent){
-                    $email_body = "<h4 style='font-weight:400'>You have gone above your set budget of &#163;$budget_round per day!<br>You have currently spent &#163;$total_spent_round.<br>To reduce the amount you spend, please consider turning off all electronic devices when not in use, or have heaters or air conditioning units on at a lower setting.<br>Better luck tomorrow!</h4>";
+                    $email_body = "<h4 style='font-weight:400'>You have gone above your set budget of &#163;$budget_round per day!<br>You have currently spent &#163;$total_spent_round.<br><br>To reduce the amount you spend, please consider turning off all electronic devices when not in use, or have heaters or air conditioning units on at a lower setting.<br>Better luck tomorrow!</h4>";
                 } else {
-                    $email_body = "<h4 style='font-weight:400'>Congratulations, you have gone kept within your set budget of &#163;$budget_round per day!<br>You have currently spent &#163;$total_spent_round.<br>Good luck for tomorrow, keep it up!</h4>";
+                    $email_body = "<h4 style='font-weight:400'>Congratulations, you have gone kept within your set budget of &#163;$budget_round per day!<br>You have currently spent &#163;$total_spent_round.<br><br>Good luck for tomorrow, keep it up!</h4>";
                 }
                 $email_signoff = "<h4 style='font-weight:400'>Kind regards,<br>The Bubble Team</h4>";
                 $email_signature = "Jamie Rice - Organisational Manager <br> Bruce Wilson - Technical Manager <br> Rory Dobson - Liaison Officer <br> Andrew MacGregor - Web Developer <br> Michael Linton - Software Engineer <br> Mark Kostryckyj - Software Developer";

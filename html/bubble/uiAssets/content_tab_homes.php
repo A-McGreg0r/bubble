eee4cfabb631e079650fae014c00f801e74a71f2
Andrew Mcgregor <mcgregor381@hotmail.co.uk>
2020-04-10 19:39:16 +0100

    fixed? chart buttons elament pointer

--- html/bubble/uiAssets/content_tab_homes.php
+++ html/bubble/uiAssets/content_tab_homes.php
@@ -351,11 +351,11 @@ function generateHomeTab()
                                                   </div>
                                             <!--Donut 3-->  
                                             <!--button for Donut carousel -->
-                                              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                              <a class="carousel-control-prev" href="#chart-carousel" role="button" data-slide="prev">
                                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                                 <span class="sr-only">Previous</span>
                                               </a>
-                                              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                              <a class="carousel-control-next" href="#chart-carousel" role="button" data-slide="next">
                                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                                 <span class="sr-only">Next</span>
                                               </a>  

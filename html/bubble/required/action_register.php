968600fae4a58fb3bb0ad7afb3b868c1f8efac79
bww1 <bww1@hw.ac.uk>
2020-04-15 18:27:52 +0100

    Fix registration email

--- html/bubble/required/action_register.php
+++ html/bubble/required/action_register.php
@@ -104,7 +104,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if($stmt->affected_rows === 1){
             echo("{\"success\":\"Registration successful\"}");
             //SEND REGISTRATION EMAIL
-            include dirname(__DIR__).'/required/email/registration_email.php';
+            require "email/registration_email.php";
         }else{
             echo("{\"error\":\"Registration failed, try again!\"}");
         }

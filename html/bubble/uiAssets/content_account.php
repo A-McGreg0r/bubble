f66ea528d8f4450aeeb3b0bcbdefe3b54625e7af
Andrew <Mcgregor381@hotmail.co.uk>
2020-04-19 17:34:09 +0100

    comments lay out fix

--- html/bubble/uiAssets/content_account.php
+++ html/bubble/uiAssets/content_account.php
@@ -147,127 +147,158 @@ function generateAccount(){
                                 
                             </div>
                         </div>
-                        <!--Model deployed by update account button-->
-                        <div class="modal fade" id="updateAccountModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <!--Model deployed by Delete account button-->
+                        <div class="modal fade" id="removeAccountModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <!-- Dialog -->
                             <div class="modal-dialog" role="document">
                                 <!-- Content -->
-                                <div class="modal-content">                                
+                                <div class="modal-content">
+                                
                                     <!--Header-->
                                     <div class="modal-header">
-                                        <h3>Update Account</h3>
+                                        <h3>Are You sure?</h3>
                                     </div>
                                     <!--Body-->
-                                       <h4 class="bold-title">Account details</h4>
-                                       <h4 class="bold-title">New details</h4>
-                    
-                                            <div class="col-lg">
-                                                <div class="row">
-                                                    <div class="col-md">
-                                                        <strong>&ensp;First Name: $first_name&ensp;</strong>
-                                                       
-                                                    </div>
-                                                    <div class="col-md">
-                                                        <input class="form-control" type="text" placeholder="$first_name">
-                                                    </div>
+                                    <div class="modal-body">
+                                        <strong> Warring:   This will completely delete your account and can't be undone!</strong>
+                                    </div>
+                                    <div class="modal-footer justify-content-lg-between">
+                                        <button class="btn btn-danger btn-sm btn-rounded" >Yes: delete account</button>
+                                        <button type="button" class="btn btn-primary btn-sm btn-rounded" data-dismiss="modal" >No: go back.</button>
+                                    </div>
+                                    
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <!--Model deployed by update account button-->
+                        <div class="modal fade" id="updateAccountModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <!-- Dialog -->
+                            <div class="modal-dialog modal-lg" role="document">
+                            <!--Header-->
+                           
+                                <!-- Content -->
+                                <div class="modal-content ">
+                                    <div class="modal-header">
+                                        <h3 class="bold-title">Update Account</h3>
+                                    </div>                                
+                                    <div class="col-lg">
+                                    <!--Body-->
+                                    <div class="row" style="margin: 0; padding-top: 5px; padding-bottom: 5px">
+                                        <div class="col-md">
+                                            <h4>Account details</h4>
+                                        </div>
+                                        
+                                        <div class="col-md">
+                                            <h4>New details</h4>
+                                        </div>
+                                    </div>
+                                    
+                                     <!--First Name-->  
+                                        <div class="row" style="padding-top: 5px; padding-bottom: 5px">
+                                            <div class="col-md">
+                                                <strong>&ensp;First Name: $first_name&ensp;</strong>
+                                            </div>
+                                                <div class="col-md">
+                                                    <input class="form-control" type="text" placeholder="$first_name">
                                                 </div>
-                                                
-                                                <div class="row">
-                                                    <div class="col-md">
-                                                        <strong>&ensp;Last Name: $last_name&ensp;</strong>
+                                        </div>
+                                        
+                                        <!--Last Name-->
+                                        <div class="row" style="padding-top: 5px; padding-bottom: 5px">
+                                            <div class="col-md">
+                                                <strong>&ensp;Last Name: $last_name&ensp;</strong>
+                                            </div>
+                                            <div class="col-md"">
+                                                <input class="form-control" type="text" placeholder="$last_name">
+                                            </div>
+                                        </div>
+                                        
+                                        <!--E-mail-->
+                                        <div class="row" style="padding-top: 5px; padding-bottom: 5px">
+                                            <div class="col-md">
+                                                <strong>&ensp;Email Address: $email&ensp;</strong><!--todo fix md size alinment issue-->
+                                            </div>
+                                            <div class="col-md">
+                                                <input class="form-control" type="text" placeholder="$email">
+                                            </div>
+                                        </div>
+                                        
+                                        <!--Address-->
+                                        <div class="row" style="margin-left: 0; padding-top: 5px; padding-bottom: 5px">
+
+                                                <div class="col-md">
+                                                    <div class="row" >
+                                                        <strong>&ensp;Address:</strong>  
                                                     </div>
-                                                    <div class="col-md"">
-                                                        <input class="form-control" type="text" placeholder="$last_name">
-                                                    </div>
-                                                </div>
-                                                
-                                                <div class="row">
-                                                    <div class="col-md">
-                                                        <strong>&ensp;Email Address: $email&ensp;</strong>
-                                                    </div>
-                                                    <div class="col-md">
-                                                        <input class="form-control" type="text" placeholder="$email">
-                                                    </div>
-                                                </div>
-                                                
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <strong>&ensp;Address:</strong>
-                                                            <div class="row">
-                                                                <strong>$address_l1&ensp;</strong>
-                                                            </div>
-                                                            
-                                                            <div class="row">
-                                                                <strong>$address_l2&ensp;</strong>
-                                                            </div>
-                                                    </div>
-                                                    
-                                                    <div class="col-md-6">
-                                                        <div class="row">
+                                                    <div class="row" style="padding-top: 5px; padding-bottom: 5px">
+                                                        <div class="col-md">
+                                                            <strong>$address_l1</strong>
+                                                        </div>
+                                                        <div class="col-md" style="margin-left: -2%;">
                                                             <input class="form-control" type="text" placeholder="$address_l1">
                                                         </div>
-                                                        
-                                                        <div class="row">
-                                                            <input class="form-control" type="text" placeholder="$address_l2">
+                                                    </div>
+                                                    
+                                                     <div class="row" style=" padding-top: 5px; padding-bottom: 5px">
+                                                        <div class="col-md">
+                                                            <strong>$address_l2</strong>
                                                         </div>
+                                                        <div class="col-md" style="margin-left: -2%;">
+                                                            <input class="form-control" type="text" placeholder="$address_l2">
+                                                        </div> 
                                                     </div>
+                                                    
                                                 </div>
-                                                
-                                                
-                                                <div class="row">
-                                                    <div class="col-md">
-                                                        <strong>&ensp;Postcode:$postcode&ensp;</strong>
-                                                    </div>
-                                                    <div class="col-md">
-                                                        <input class="form-control" type="text" placeholder="$postcode">
-                                                    </div>
-                                                </div>
-                                                
-                                                <div class="row ">
-                                                    <div class="col-md">
-                                                        <strong>&ensp;Energy Price: $energy_cost&ensp;</strong>
-                                                    </div>
-                                                    <div class="col-md">
-                                                        <input class="form-control" type="text" placeholder="$energy_cost">
-                                                    </div>
-                                                </div>
-                                                
-                                                <div class="row ">
-                                                    <div class="col-md">
-                                                        <strong>&ensp;Monthly Budget: $budget&ensp;</strong>
-                                                    </div>
-                                                    <div class="col-md">
-                                                        <input class="form-control" type="text" placeholder="$budget">
-                                                    </div>
-                                                </div>
-                                                
-                                                <div class="row ">
-                                                    <div class="col-md">
-                                                        <strong>&ensp;Solar Panel Rating: $solargen&ensp;</strong>
-                                                    </div>
-                                                    <div class="col-md">
-                                                        <input class="form-control" type="text" placeholder="$solargen">
-                                                    </div>
-                                                </div>
-                                            
-                                                <div class="row" style="margin 50px" style="min-width: 120px">
-                                                    <div class="col-md">
-                                                        <button type="button" class="btn-sm btn-primary  btn-rounded" data-toggle="modal" data-target="#updateAccountModal">Update Account</button>
-                                                    </div>
-                                                    <div class="col-md">
-                                                        <button type="button" class="btn-sm btn-danger  btn-rounded" data-toggle="modal" data-target="#removeAccountModal">Delete Account</button>
-                                                    </div>
-                                                </div>
-                                                            
+                                    
+                      
+                                        </div>
+
+                                        <!--Postcode-->
+                                        <div class="row" style="padding-top: 5px; padding-bottom: 5px">
+                                            <div class="col-md">
+                                                <strong>&ensp;Postcode:$postcode&ensp;</strong>
                                             </div>
+                                                <div class="col-md">
+                                                    <input class="form-control" type="text" placeholder="$postcode">
+                                                </div>
+                                        </div>
+                                                
+                                        <div class="row" style="padding-top: 5px; padding-bottom: 5px">
+                                            <div class="col-md">
+                                                <strong>&ensp;Energy Price: $energy_cost&ensp;</strong>
+                                            </div>
+                                                <div class="col-md">
+                                                    <input class="form-control" type="text" placeholder="$energy_cost">
+                                                </div>
+                                        </div>
+                                                
+                                        <div class="row" style="padding-top: 5px; padding-bottom: 5px">
+                                            <div class="col-md">
+                                                <strong>&ensp;Monthly Budget: $budget&ensp;</strong>
+                                            </div>
+                                                <div class="col-md">
+                                                    <input class="form-control" type="text" placeholder="$budget">
+                                                </div>
+                                        </div>
+                                        <!--Model footer-->    
+                                        <div class="modal-footer justify-content-lg-between" style="margin 50px">
+                                            <div class="col-md">
+                                                  <button class="btn btn-danger btn-sm btn-rounded" data-dismiss="modal" >Cancel</button>
+                                        
+                                            </div>
+                                                <div class="col-md">
+                                                    <button type="button" class="btn btn-primary btn-sm btn-rounded" onclick="" >Update</button>
+                                                </div>
+                                        </div>
+                                                            
+                                </div>
                                 
                             </div>
                         </div>
                     <!--col om to center content-->
-                    </div>
-                <!-- Card content: account details -->    
-                </div>                
-            <!-- Card End-->       
+                        </div>
+                <!-- modle End--> 
            
                    
             

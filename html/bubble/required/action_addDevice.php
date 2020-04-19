69485b917e22599b5df0249a60e5bddf5af3d563
bww1 <bww1@hw.ac.uk>
2020-04-19 13:34:07 +0100

    Fixing adding devices

--- html/bubble/required/action_addDevice.php
+++ html/bubble/required/action_addDevice.php
@@ -70,94 +70,94 @@ error_reporting(E_ALL);
         //SWITCH FOR DEVICE TYPE
         switch($deviceType){
             case 0: //HUB
-                // //FIND HUB IN TABLE WITH SPESIFIED AUTH_KEY
-                // $stmt = $db->prepare("SELECT * FROM hub_info WHERE auth_key = ?");
-                // $stmt->bind_param("s", $auth_key);
-                // $stmt->execute();
-                // $result = $stmt->get_result();
-                // if ($result->num_rows > 0) {
-                //     //INSERT LINK IN HUB_USERS BETWEEN NEWLY ADDED HUB AND USER_ID FROM LOGGED IN SESSION
-                //     $row = $result->fetch_assoc();
-                //     $hub_id = $row['hub_id'];
+                //FIND HUB IN TABLE WITH SPESIFIED AUTH_KEY
+                $stmt = $db->prepare("SELECT * FROM hub_info WHERE auth_key = ?");
+                $stmt->bind_param("s", $auth_key);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if ($result->num_rows > 0) {
+                    //INSERT LINK IN HUB_USERS BETWEEN NEWLY ADDED HUB AND USER_ID FROM LOGGED IN SESSION
+                    $row = $result->fetch_assoc();
+                    $hub_id = $row['hub_id'];
 
-                //     //CHECK IF THE HUB ALREADY HAS AN OWNER
-                //     $stmtOwner = $db->prepare("SELECT * FROM hub_owners WHERE hub_id = ?");
-                //     $stmtOwner->bind_param("i", $hub_id);
-                //     $stmtOwner->execute();
-                //     $resultOwner = $stmtOwner->get_result();
+                    //CHECK IF THE HUB ALREADY HAS AN OWNER
+                    $stmtOwner = $db->prepare("SELECT * FROM hub_owners WHERE hub_id = ?");
+                    $stmtOwner->bind_param("i", $hub_id);
+                    $stmtOwner->execute();
+                    $resultOwner = $stmtOwner->get_result();
 
-                //     //IF THE HUB DOESNT HAVE AN OWNER
-                //     if($resultOwner->num_rows == 0){
+                    //IF THE HUB DOESNT HAVE AN OWNER
+                    if($resultOwner->num_rows == 0){
 
-                //         //ADD OWNER IN OWNERS TABLE
-                //         $stmtAddOwner = $db->prepare("INSERT INTO hub_owners (hub_id,hub_owner_id) VALUES (?, ?)");
-                //         $stmtAddOwner->bind_param("ii", $hub_id, $user_id);
-                //         if(!$stmtAddOwner->execute()){
-                //             echo("{\"error\":\"Unknown error, contact support\"}");
-                //             $stmtAddOwner->close();
-                //             $stmtOwner->close();
-                //             $stmt->close();
-                //             exit(0;)
-                //         }
-                //         $stmtAddOwner->close();
+                        //ADD OWNER IN OWNERS TABLE
+                        $stmtAddOwner = $db->prepare("INSERT INTO hub_owners (hub_id,hub_owner_id) VALUES (?, ?)");
+                        $stmtAddOwner->bind_param("ii", $hub_id, $user_id);
+                        if(!$stmtAddOwner->execute()){
+                            echo("{\"error\":\"Unknown error, contact support\"}");
+                            $stmtAddOwner->close();
+                            $stmtOwner->close();
+                            $stmt->close();
+                            exit(0);
+                        }
+                        $stmtAddOwner->close();
 
-                //         //INSERT JOIN BETWEEN USER AND ACCOUNT
-                //         $stmt1 = $db->prepare("INSERT INTO hub_users (user_id, hub_id) VALUES (?, ?)");
-                //         $stmt1->bind_param("ii", $user_id, $hub_id);
-                //         if ($stmt1->execute()) {
-                //             echo("{\"success\":\"Sucessfully registered your new hub!\"}");
-                //         }else{
-                //             echo("{\"error\":\"Unknown error, contact support\"}");
-                //         }
-                //         $stmt1->close(); 
-                //     } else {//THE HUB HAS AN OWNER, REQUEST ACCESS FROM OWNER
-                //         echo("{\"success\":\"An access request to the owner of this hub has been sent! Please ask them to check the email associated with their account, and confirm your access.!\"}");
-                //     }
-                //     $stmtOwner->close();
-                // } else {
-                //     echo("{\"error\":\"This Hub doesnt appear registered with us yet. Ensure the device has a green flashing LED on the top.\"}");
-                // }
-                // $stmt->close();
+                        //INSERT JOIN BETWEEN USER AND ACCOUNT
+                        $stmt1 = $db->prepare("INSERT INTO hub_users (user_id, hub_id) VALUES (?, ?)");
+                        $stmt1->bind_param("ii", $user_id, $hub_id);
+                        if ($stmt1->execute()) {
+                            echo("{\"success\":\"Sucessfully registered your new hub!\"}");
+                        }else{
+                            echo("{\"error\":\"Unknown error, contact support\"}");
+                        }
+                        $stmt1->close(); 
+                    } else {//THE HUB HAS AN OWNER, REQUEST ACCESS FROM OWNER
+                        echo("{\"success\":\"An access request to the owner of this hub has been sent! Please ask them to check the email associated with their account, and confirm your access.!\"}");
+                    }
+                    $stmtOwner->close();
+                } else {
+                    echo("{\"error\":\"This Hub doesnt appear registered with us yet. Ensure the device has a green flashing LED on the top.\"}");
+                }
+                $stmt->close();
 
             break;
             default: //OTHER DEVICE
-                // //BEGIN SESSION
-                // session_start();
-                // //GRAB USER_ID
-                // $hub_id = $_SESSION['hub_id'];
-                // //END SESSION
-                // session_write_close();
+                //BEGIN SESSION
+                session_start();
+                //GRAB USER_ID
+                $hub_id = $_SESSION['hub_id'];
+                //END SESSION
+                session_write_close();
 
-                // //SANITIZE DEVICE TYPE, CHECK THE DEVICE IS A KNOWN DEVICE TYPE
-                // $stmtCheckType = $db->prepare("SELECT * FROM device_types WHERE type_id = ?");
-                // $stmtCheckType->bind_param("i", $deviceType);
-                // if(!$stmtCheckType->execute()){
-                //     echo("{\"error\":\"Invalid device type\"}");
-                //     $stmtCheckType->close();
-                //     exit(0);
-                // }
-                // $result = $stmtCheckType->get_result();
-                // if($result->num_rows != 1){
-                //     echo("{\"error\":\"Unknown device type\"}");
-                //     $stmtCheckType->close();
-                //     exit(0);
-                // }
-                // //GET THE NAME OF THE DEVICE FOR DEFAULT NAME
-                // $row = $result->fetch_assoc();
-                // $device_name = $row['type_name'];
-                // $stmtCheckType->close();
+                //SANITIZE DEVICE TYPE, CHECK THE DEVICE IS A KNOWN DEVICE TYPE
+                $stmtCheckType = $db->prepare("SELECT * FROM device_types WHERE type_id = ?");
+                $stmtCheckType->bind_param("i", $deviceType);
+                if(!$stmtCheckType->execute()){
+                    echo("{\"error\":\"Invalid device type\"}");
+                    $stmtCheckType->close();
+                    exit(0);
+                }
+                $result = $stmtCheckType->get_result();
+                if($result->num_rows != 1){
+                    echo("{\"error\":\"Unknown device type\"}");
+                    $stmtCheckType->close();
+                    exit(0);
+                }
+                //GET THE NAME OF THE DEVICE FOR DEFAULT NAME
+                $row = $result->fetch_assoc();
+                $device_name = $row['type_name'];
+                $stmtCheckType->close();
 
-                // //PREPARE NEW DEVICE INSERTION
-                // $stmtNewDevice = $db->prepare("INSERT INTO device_info (hub_id, device_auth_code, device_name, device_type, device_status) VALUES (?,?,?,?,0)");
-                // $stmtNewDevice->bind_param("issi", $hub_id, $auth_key, $device_name, $deviceType);
-                // if(!$stmtNewDevice->execute()){
-                //     echo("{\"error\":\"Unknown error, please try again\"}");
-                //     $stmtNewDevice->close();
-                //     exit(0);
-                // }
-                // echo("{\"success\":\"Device successfully added\"}");
-                // $stmtNewDevice->close();
-                // exit(0);
+                //PREPARE NEW DEVICE INSERTION
+                $stmtNewDevice = $db->prepare("INSERT INTO device_info (hub_id, device_auth_code, device_name, device_type, device_status) VALUES (?,?,?,?,0)");
+                $stmtNewDevice->bind_param("issi", $hub_id, $auth_key, $device_name, $deviceType);
+                if(!$stmtNewDevice->execute()){
+                    echo("{\"error\":\"Unknown error, please try again\"}");
+                    $stmtNewDevice->close();
+                    exit(0);
+                }
+                echo("{\"success\":\"Device successfully added\"}");
+                $stmtNewDevice->close();
+                exit(0);
             break;
         }
 

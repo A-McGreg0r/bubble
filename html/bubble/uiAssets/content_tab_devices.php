6b9498c76bbc8658293584939fd87a68c5f70eda
bww1 <bww1@hw.ac.uk>
2020-04-19 22:40:17 +0100

    Working on access tokens

--- html/bubble/uiAssets/content_tab_devices.php
+++ html/bubble/uiAssets/content_tab_devices.php
@@ -2,7 +2,9 @@
 <?php
 include_once dirname(__DIR__).'/required/config.php';
 include_once "content_addDevice.php";
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 function generateDeviceTab(){
     global $db;
     //ADD NEW DEVICE CARD, GENERATE ALWAYS AT TOP!

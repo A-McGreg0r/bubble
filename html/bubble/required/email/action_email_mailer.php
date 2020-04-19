87e9dbe1fae4202e9bc22fbe5b6d4cbd2608ceb0
bww1 <bww1@hw.ac.uk>
2020-04-19 14:21:55 +0100

    Working on access requests

--- html/bubble/required/email/action_email_mailer.php
+++ html/bubble/required/email/action_email_mailer.php
@@ -1,4 +1,7 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/autoload.php';
 include_once dirname(__DIR__).'/config.php';

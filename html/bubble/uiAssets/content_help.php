42e09814fbb003e800f1089eefeb9e876ea33b49
rmd5 <rmd5@hw.ac.uk>
2020-04-21 22:12:48 +0100

    Adding modals

--- html/bubble/uiAssets/content_help.php
+++ html/bubble/uiAssets/content_help.php
@@ -101,8 +101,21 @@ function generateHelpPage() {
                         </td>
                     </tr>
                     <tr>
-                        <td>
+                        <td onclick="openModalHelp('help5')">
                             What is the advice for?
+                        </td>
+                    </tr>
+                    <tr style="display:none!important" id="help5">
+                        <td>
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help5')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">What is the advice for?</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
                         </td>
                     </tr>
 
@@ -110,61 +123,189 @@ function generateHelpPage() {
                 <div class="section-title"><h4>Room Tab</h4></div>
                 <table class="helpTable">
                     <tr>
-                        <td>
+                        <td onclick="openModalHelp('help6')">
                             Turn on a room
                         </td>
                     </tr>
-                    <tr>
+                    <tr style="display:none!important" id="help6">
                         <td>
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help6')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Turn on a room</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td onclick="openModalHelp('help7')">
                             Setting a timer
                         </td>
                     </tr>
-                    <tr>
+                    <tr style="display:none!important" id="help7">
                         <td>
-                            Adding a room
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help7')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Setting a timer</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
                         </td>
                     </tr>
                     <tr>
+                        <td onclick="openModalHelp('help8')">
+                            Adding a room
+                        </td>
+                    </tr>
+                    <tr style="display:none!important" id="help8">
                         <td>
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help8')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Adding a room</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td onclick="openModalHelp('help9')">
                             Removing a room
+                        </td>
+                    </tr>
+                    <tr style="display:none!important" id="help9">
+                        <td>
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help9')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Removing a room</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
                         </td>
                     </tr>
                 </table>
                 <div class="section-title"><h4>Device Tab</h4></div>
                 <table class="helpTable">
                     <tr>
+                        <td onclick="openModalHelp('help10')">
+                            Setting a timer on a device
+                        </td>
+                    </tr>
+                    <tr style="display:none!important" id="help10">
                         <td>
-                            Setting a timer
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help10')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Setting a timer</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
                         </td>
                     </tr>
                     <tr>
-                        <td>
+                        <td onclick="openModalHelp('help11')">
                             Review my device statistics
                         </td>
                     </tr>
-                    <tr>
+                    <tr style="display:none!important" id="help11">
                         <td>
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help11')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Review my device statistics</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td onclick="openModalHelp('help12')">
                             Adding a device
                         </td>
                     </tr>
-                    
-                    <tr>
+                    <tr style="display:none!important" id="help12">
                         <td>
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help12')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Adding a device</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td onclick="openModalHelp('help13')">
                             Moving a device to a different room
                         </td>
                     </tr>
-                    <tr>
+                    <tr style="display:none!important" id="help13">
                         <td>
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help13')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Moving a device to a different room</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td onclick="openModalHelp('help14')">
                             Removing a device
                         </td>
                     </tr>
-                    
+                    <tr style="display:none!important" id="help14">
+                        <td>
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help14')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Removing a device</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
                 </table>
                 <div class="section-title"><h4>Acount</h4></div>
                 <table class="helpTable">
                     <tr>
-                        <td>
+                        <td onclick="openModalHelp('help15')">
                             Change my account details
+                        </td>
+                    </tr>
+                    <tr style="display:none!important" id="help15">
+                        <td>
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" onclick="openModalHelp('help15')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Change my account details</h3>
+                                </div>
+                                <div class="modalBody">
+                                    Hello
+                                </div>
+                            </div>
                         </td>
                     </tr>
                     <tr>

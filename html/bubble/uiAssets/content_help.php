de989b4cca55d1e9d2febff016c93644814ffd1b
rmd5 <rmd5@hw.ac.uk>
2020-04-22 02:08:55 +0100

    Added more content

--- html/bubble/uiAssets/content_help.php
+++ html/bubble/uiAssets/content_help.php
@@ -42,7 +42,7 @@ function generateHelpPage() {
                                         will turn off all devices in your home. If you would like to set a timer that will
                                         automatically turn off your home, click on the clock icon in the top right of the button.
                                         The timer icon will only be visible if there is currently a device turned on.</div>
-                                    <img src="img/turnOffHome.png" style="max-width:90%;"/>
+                                    <br><img src="img/turnOffHome.png" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>
@@ -64,7 +64,7 @@ function generateHelpPage() {
                                         clicking on the 'Change House' button on the home page. Clicking on this will open a list 
                                         of all of your homes, and by selecting one, you can view all of the statistics, rooms and
                                         devices associated with that hub.</div>
-                                    <img src="img/changeHome.png" style="max-width:90%;"/>
+                                    <br><img src="img/changeHome.png" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>
@@ -86,7 +86,7 @@ function generateHelpPage() {
                                         click on the 'Change Costing' button on the home page. This will open a form that you can fill
                                         in to change the stored values of each. If you don't want to change one of the values, just 
                                         just leave that part of the form empty.</div>
-                                    <img src="img/changeCosting.png" style="max-width:90%;"/>
+                                    <br><img src="img/changeCosting.png" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>
@@ -161,7 +161,7 @@ function generateHelpPage() {
                                         <br><br>Similarly, if you would like to turn off all of the devices in the room, 
                                         click the room. The button will turn grey to indicate it is turning off, the loading icon
                                         will appear, then the status will update to 'off'.</div>
-                                    <img src="img/turnOnRoom.JPG" style="max-width:90%;"/>
+                                    <br><img src="img/turnOnRoom.JPG" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>
@@ -183,7 +183,7 @@ function generateHelpPage() {
                                         Clicking on this button will open up a small window that will allow you to set a timer that
                                         will schedule the devices in that room to turn off. It will only set timers for devices that
                                         are currently turned on, and any timer that has been set will be deleted if the room is turned off.</div>
-                                    <img src="img/roomTimer.png" style="max-width:90%;"/>
+                                    <br><img src="img/roomTimer.png" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>
@@ -204,7 +204,7 @@ function generateHelpPage() {
                                     <div>In order to add a new room, there is a button at the very top of the Room Tab. Clicking on this button
                                         will open up a small form in which you can input the name of the room and the icon you would like
                                         it to have. Click on 'Add Room' at the bottom, and the new room will be added to the list.</div>
-                                    <img src="img/addRoom.png" style="max-width:90%;"/>
+                                    <br><img src="img/addRoom.png" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>
@@ -226,7 +226,7 @@ function generateHelpPage() {
                                     This will open up the statistics popup for the room. At the bottom of this popup, there
                                     is a button that says 'Delete Room'. Click this button and another popup will appear asking for
                                     confirmation. Click 'Delete Room', and the room will be deleted from your account.</div>
-                                    <img src="img/removeRoom.png" style="max-width:90%;"/>
+                                    <br><img src="img/removeRoom.png" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>
@@ -234,6 +234,32 @@ function generateHelpPage() {
                 </table>
                 <div class="section-title"><h4>Device Tab</h4></div>
                 <table class="helpTable">
+                    <tr>
+                        <td style="cursor:pointer" onclick="openModalHelp('help20')">
+                            Turn on a device
+                        </td>
+                    </tr>
+                    <tr style="display:none!important" id="help20">
+                        <td>
+                            <div class="modalStats" style="border: 1px solid black;" >
+                                <div class="x-adjust"><i class="stats_icon_x " id="" style="display:block" style="cursor:pointer" onclick="openModalHelp('help20')"><i class="fas fa-times"></i></i></div>
+                                <div class="modalHeader">
+                                    <h3 class="bold-title">Turn on a device</h3>
+                                </div>
+                                <div class="modalBody">
+                                    <div>To turn on a device, simply click on the device you would like to turn on.
+                                        A loading symbol will appear and the device button will refresh with it's new status.
+                                        If a device button is yellow, then it is turned on. If it is grey, then it is turned off.
+                                        <br><br>
+                                        Some devices, such as heaters and air conditioners, have dimmer switches. A dimmer switch
+                                        will display their current setting in sections of yellow, as seen below. The status to the right of the
+                                        button will display the current setting of the device, which varies between 0 and 4. Clicking
+                                        on these buttons will increase the setting of the device by one, then drop back down to 0.</div>
+                                        <br><img src="img/turnOnDevice.JPG" style="max-width:90%;"/>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
                     <tr>
                         <td style="cursor:pointer" onclick="openModalHelp('help10')">
                             Setting a timer on a device
@@ -247,7 +273,11 @@ function generateHelpPage() {
                                     <h3 class="bold-title">Setting a timer</h3>
                                 </div>
                                 <div class="modalBody">
-                                    Hello
+                                    <div>When a device is turned on, a clock icon will appear in the top right corner of the button.
+                                        Clicking on this button will open up a small popup in which you can set a timer to turn off your device.
+                                        The time that the device is scheduled to turn off will appear. If the device is turned off before
+                                        the timer turns it off, the timer will be cancelled.</div>
+                                    <br><img src="img/deviceTimer.png" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>
@@ -265,7 +295,14 @@ function generateHelpPage() {
                                     <h3 class="bold-title">Review my device statistics</h3>
                                 </div>
                                 <div class="modalBody">
-                                    Hello
+                                    <div>To access the statistics of a device, click on the information icon in the top left of the button.
+                                        Opening this popup, you will see a pie chart that shows a comparison between the amount this device
+                                        is costing you compared to all other devices put together. Underneath, you will see the numbers laid
+                                        out along with a percentage for this device.
+                                        <br><br>
+                                        Underneath, there is an energy usage section that shows the energy consumption of the device over 
+                                        the past hour, day, month and year, and how much each has cost you.</div>
+                                    <br><img src="img/deviceStats.png" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>
@@ -283,7 +320,11 @@ function generateHelpPage() {
                                     <h3 class="bold-title">Adding a device</h3>
                                 </div>
                                 <div class="modalBody">
-                                    Hello
+                                    <div>To add a new device, click on the 'Add new device' button at the top of the devics tab.
+                                        This will open up a popup, and, assuming you have allowed access to your camera, will show
+                                        the view from your camera. Scan the QR code that came with your device, and the device will
+                                        automatically be linked to your account. That's all there is to it!</div>
+                                    <br><img src="img/addDevice.png" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>
@@ -302,6 +343,7 @@ function generateHelpPage() {
                                 </div>
                                 <div class="modalBody">
                                     Hello
+                                    <br><img src="img/deviceStats.png" style="max-width:90%;"/>
                                 </div>
                             </div>
                         </td>

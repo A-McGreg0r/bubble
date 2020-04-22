4cc415ac9071aeec63fdb02c5f1d361e761b394f
bww1 <bww1@hw.ac.uk>
2020-04-22 12:43:54 +0100

    Updating modals

--- html/bubble/uiAssets/content_addDevice.php
+++ html/bubble/uiAssets/content_addDevice.php
@@ -18,9 +18,11 @@ html;
     }
     $html .= <<<pageHTML
 
-    <div class="modal modalStatsWrap" id="addDeviceModal" tabindex="-1" role="dialog" aria-labelledby="addDeviceModalLabel" aria-hidden="true">
-        <div class="modalStats modalContent" >
-            <div class="x-adjust"><i class="stats_icon_x " id="" style="display:flex" onclick="$('#addDeviceModal').modal().toggle();"><i class="fas fa-times"></i></i></div>
+    <div class="modal fade modalStatsWrap" id="addDeviceModal" tabindex="-1" role="dialog" aria-labelledby="addDeviceModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modalStats modalContent" >
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <i class="fas fa-times"></i>
+            </button>
             <div class="modalHeader"><strong>Add a new device</strong></div>
             <div class="modal-body">
                 <div class="container">
@@ -51,8 +53,50 @@ html;
         </div>
     </div>
 
-   
+
 pageHTML;
 
     return $html;
+
+    
+    
+    // <div class="modal fade" id="addDeviceModal" tabindex="-1" role="dialog" aria-labelledby="addDeviceModalLabel" aria-hidden="true">
+    //     <div class="modal-dialog modal-dialog-centered" role="document">
+    //         <div class="modal-content">
+    //             <div class="modal-header">
+    //                 <h5 class="modal-title" id="addDeviceModalLabel">Add a new device</h5>
+    //                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    //                 <span aria-hidden="true">&times;</span>
+    //                 </button>
+    //             </div>
+    //             <div class="modal-body">
+    //                 <div class="container">
+    //                     <div class="col-lg-12">
+    //                         <div class="row justify-content-center">
+    //                             <div class="text-center align-middle">
+    //                                 <h3>Add a new hub or device by scanning the QR code on the device</h3>
+    //                                 <h5>Hub already setup on a different account? No worries, scan the qr code below and we will send a request to your hub's owner to allow you to connect</h5>
+    //                                 <div id="devicetext" style="width:100%"></div>
+    //                             </div>
+    //                             <div class="align-middle">
+    //                                 <div style="position:absolute; left: 50%; top:50%; transform: translate(-50%, -50%);">
+    //                                     <div style="visibility: hidden" id="loading" class="spinner-border text-primary" role="status">
+    //                                         <span class="sr-only">Loading...</span>
+    //                                     </div>
+    //                                 </div>
+    //                                 <div style="position:absolute; left: 50%; top:90%; transform: translate(-50%, -50%);">
+    //                                     <button type="button" onclick="submitImage();" id="submitImage">
+    //                                         <i class="fas fa-dot-circle fa-5x"></i>
+    //                                     </button>
+    //                                 </div>
+    //                                 <video autoplay="true" id="videoElement" style="width:100%"></video>
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //                 </div>                
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
+   
 }
